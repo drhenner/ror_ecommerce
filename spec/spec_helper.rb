c628faf43e3e7a505d04ea698f1f32a5bff3fdba
David Henner <davidhenner@drhenner.local>
2010-10-18 17:30:38 +0000

    good news and bad news...  tests are running correctly now... bad news  TESTs are failling.  its time to clean this up

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,12 +1,15 @@
-# This file is copied to ~/spec when you run 'ruby script/generate rspec'
-# from the project root directory.
+# This file is copied to spec/ when you run 'rails generate rspec:install'
 ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
 # Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+# in spec/support/ and its subdirectories.
+#Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+
 include Hadean::TruncateHelper
 
 RSpec.configure do |config|
@@ -19,13 +22,14 @@ RSpec.configure do |config|
   # config.mock_with :rr
   config.mock_with :rspec
 
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
-
   config.before(:suite) { trunctate_unseeded }
-  
+
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
-  # examples within a transaction, comment the following line or assign false
+  # examples within a transaction, remove the following line or assign false
   # instead of true.
-  #config.use_transactional_fixtures = true
+  config.use_transactional_fixtures = true
   #config.logger = :stdout
 end
