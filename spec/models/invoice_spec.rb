60c025c51bca8f239a73c2ad492208f7adc3f288
David Henner <davidhenner@drhenner.local>
2010-10-25 05:26:56 +0000

    cancel accounting transactions

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -21,6 +21,7 @@ describe Invoice, "instance methods" do
   end
   
   context ".capture_complete_order" do
+    pending "these tests all can be more thorough... LIKE ensuring credit and debit add to 0"
     it 'should create a CreditCardCapture transaction' do
       @invoice.stubs(:amount).returns(20.50)
       #@invoice.stubs(:batches).returns([])
@@ -36,12 +37,34 @@ describe Invoice, "instance methods" do
         @invoice.capture_complete_order.should be_true
         @invoice.order.user.transaction_ledgers.size.should == 4
       end
+      
+      context 'cancel_authorized_payment' do
+        it 'should create a CreditCardReceivePayment transaction then cancel' do
+          @invoice.stubs(:amount).returns(20.50)      
+          @invoice.authorize_complete_order
+        
+          @invoice.cancel_authorized_payment.should be_true
+          @invoice.order.user.transaction_ledgers.size.should == 4
+          revenue_credits = ar_credits = revenue_debits = ar_debits = []
+          @invoice.order.user.transaction_ledgers.each do |ledger|
+            if ledger.transaction_account_id == TransactionAccount::REVENUE_ID
+              revenue_credits << ledger.credit
+              revenue_debits  << ledger.debit
+            end
+            if ledger.transaction_account_id == TransactionAccount::ACCOUNTS_RECEIVABLE_ID
+              ar_credits << ledger.credit
+              ar_debits  << ledger.debit
+            end
+          end
+          ## credits and debits should cancel themselves out
+          revenue_credits.sum.should_not == 0
+          ar_credits.sum.should_not == 0
+          revenue_credits.sum.should  == revenue_debits.sum
+          ar_credits.sum.should       == ar_debits.sum
+        end
+      end
     end
   end
-end
-
-describe Invoice, ".cancel_authorized_payment" do
-  pending "test for cancel_authorized_payment"
 end
 
 describe Invoice, "#process_rma(return_amount, order)" do
