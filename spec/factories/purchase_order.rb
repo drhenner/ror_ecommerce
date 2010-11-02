00cb5e400c51a5f91bf69bd1f548bfcdcb9589cd
David Henner <davidhenner@drhenner.local>
2010-11-02 05:06:18 +0000

    PurchaseOrderVariant tests are completed

--- spec/factories/purchase_order.rb
+++ spec/factories/purchase_order.rb
@@ -3,6 +3,6 @@ Factory.define :purchase_order do |u|
   u.invoice_number 'John'
   u.tracking_number nil
   u.ordered_at            { Time.now }
-  
+  u.supplier_id      1
   u.estimated_arrival_on  { Time.now.to_date }
 end
--- spec/models/purchase_order_variant_spec.rb
+++ spec/models/purchase_order_variant_spec.rb
@@ -1,13 +1,48 @@
 require 'spec_helper'
 
-describe PurchaseOrderVariant, ".receive!" do
-  pending "test for receive!"
-end
+describe PurchaseOrderVariant, "instance methods" do
+  before(:each) do
+    @variant                = Factory(:variant, :count_on_hand => 100)
+    @purchase_order_variant = Factory(:purchase_order_variant, :variant => @variant, :quantity => 10)
+  end
 
-describe PurchaseOrderVariant, ".receive_po=(answer)" do
-  pending "test for receive_po=(answer)"
-end
+  context ".receive!" do
+    it 'should call receive if true' do
+      @purchase_order_variant.receive!
+      @purchase_order_variant.variant.count_on_hand.should == 110
+    end
+    
+    it 'should mark purchase order complete' do
+      @purchase_order_variant.purchase_order.expects(:mark_as_complete).once
+      @purchase_order_variant.receive!
+    end
+  end
 
-describe PurchaseOrderVariant, ".receive_po" do
-  pending "test for receive_po"
+  context ".receive_po=(answer)" do
+    it 'should call receive if true' do
+      @purchase_order_variant.expects(:receive!).once
+      @purchase_order_variant.receive_po=('true')
+    end
+    it 'should call receive if 1' do
+      @purchase_order_variant.expects(:receive!).once
+      @purchase_order_variant.receive_po=('1')
+    end
+    it 'should call receive if 0' do
+      @purchase_order_variant.expects(:receive!).never
+      @purchase_order_variant.receive_po=('0')
+    end
+  end
+
+  context ".receive_po" do
+    #is_received
+    it 'should be true' do
+      @purchase_order_variant.is_received = true
+      @purchase_order_variant.receive_po.should be_true
+    end
+
+    it 'should call receive if 0' do
+      @purchase_order_variant.is_received = false
+      @purchase_order_variant.receive_po.should be_false
+    end
+  end
 end
