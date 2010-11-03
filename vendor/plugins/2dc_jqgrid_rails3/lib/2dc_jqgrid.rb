4f29e5eab902146b55ada8c971a0cc39fb6cc9cd
David Henner <davidhenner@drhenner.local>
2010-11-03 06:12:26 +0000

    take out JS that isn't or at least shouldnt be used

--- vendor/plugins/2dc_jqgrid_rails3/lib/2dc_jqgrid.rb
+++ vendor/plugins/2dc_jqgrid_rails3/lib/2dc_jqgrid.rb
@@ -7,7 +7,7 @@ module Jqgrid
 
     def jqgrid_javascripts
       locale = I18n.locale rescue :en
-      js = javascript_include_tag('jquery-ui-1.8.4.custom.min.js') + "\n"
+      js = javascript_include_tag('jquery-ui-1.8.5.custom.min.js') + "\n"
       js << javascript_include_tag("jqgrid/i18n/grid.locale-#{locale}.js") + "\n"
       js << javascript_include_tag('jqgrid/jquery.jqGrid.min.js') + "\n"
       # Don't know if we need it, if smth not working, just uncomment it
