cda2301d3da1847c80e82a2d79a92990de503e62
David Henner <davidhenner@drhenner.local>
2010-11-07 06:06:32 +0000

    save the cart if it hasn't been saved

--- app/controllers/shopping/cart_items_controller.rb
+++ app/controllers/shopping/cart_items_controller.rb
@@ -43,20 +43,20 @@ class Shopping::CartItemsController < Shopping::BaseController
   # POST /shopping/cart_items.xml
   def create
     @cart_item = get_new_cart_item
-    
-      if cart_item = session_cart.add_variant(params[:cart_item][:variant_id], most_likely_user)
-        flash[:notice] = [I18n.t('out_of_stock_notice'), I18n.t('item_saved_for_later')].compact.join(' ') unless cart_item.shopping_cart_item?
-        session_cart.save_user(most_likely_user)
-        redirect_to(shopping_cart_items_url)
+    session_cart.save if session_cart.new_record?
+    if cart_item = session_cart.add_variant(params[:cart_item][:variant_id], most_likely_user)
+      flash[:notice] = [I18n.t('out_of_stock_notice'), I18n.t('item_saved_for_later')].compact.join(' ') unless cart_item.shopping_cart_item?
+      session_cart.save_user(most_likely_user)
+      redirect_to(shopping_cart_items_url)
+    else
+      variant = Variant.includes(:product).find_by_id(params[:cart_item][:variant_id])
+      if variant
+        redirect_to(product_url(variant.product))
       else
-        variant = Variant.includes(:product).find_by_id(params[:cart_item][:variant_id])
-        if variant
-          redirect_to(product_url(variant.product))
-        else
-          flash[:notice] = "Sorry something went wrong"
-          redirect_to(root_url())
-        end 
-      end
+        flash[:notice] = "Sorry something went wrong"
+        redirect_to(root_url())
+      end 
+    end
   end
 
   # PUT /carts/1
