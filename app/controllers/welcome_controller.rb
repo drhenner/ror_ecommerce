bf5463e351becb0493a14227f29b0793aa23ceca
David Henner <davidhenner@drhenner.local>
2010-11-17 15:11:48 +0000

    setup is confusing if the user cant go to the hoempage.  Need to delete this code once your site is ready to go...  Add this to the readme.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,6 +4,12 @@ class WelcomeController < ApplicationController
     @featured_product = Product.featured
     @best_selling_products = Product.limit(4)
     @other_products  ## search 2 or 3 categories (maybe based on the user)
-    #redirect_to admin_merchandise_products_url unless @featured_product
+    unless @featured_product
+      if current_user && current_user.admin?
+        redirect_to admin_merchandise_products_url 
+      else
+        redirect_to login_url
+      end
+    end
   end
 end
