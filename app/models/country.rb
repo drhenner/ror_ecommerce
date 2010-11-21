41b0e6c0cbc07decc0366c5c220d385b07f1ea08
David Henner <davidhenner@drhenner.local>
2010-11-21 03:05:38 +0000

    add methods to get country select in the forms

--- app/models/country.rb
+++ app/models/country.rb
@@ -8,4 +8,19 @@ class Country < ActiveRecord::Base
   USA_ID    = 214
   CANADA_ID = 35
   
+  def abbreviation_name(append_name = "")
+    ([abbreviation, name].join(" - ") + " #{append_name}").strip
+  end
+
+  def abbrev_and_name
+    abbreviation_name
+  end
+  
+  # Finds all the countries for a form select .
+  #
+  # @param none
+  # @return [Array] an array of arrays with [string, country.id]
+  def self.form_selector
+    find(:all, :order => 'abbreviation ASC').collect { |c| [c.abbrev_and_name, c.id] }
+  end
 end
--- spec/models/country_spec.rb
+++ spec/models/country_spec.rb
@@ -10,4 +10,36 @@ describe Country do
     end
     
   end
+  
+  
+  context "#form_selector" do
+    @countries = Country.form_selector
+    @countries.class.should              == Array
+    @countries.first.class.should        == Array
+    @countries.first.first.class.should  == String
+    @countries.first.last.class.should   == Fixnum
+  end
+end
+
+describe Country do
+  before(:each) do
+    @country ||= Country.new( :abbreviation => 'US', :name => 'United States')
+  end
+  
+  context ".abbreviation_name(append_name = )" do
+  
+    it 'should return the correct string with no params' do
+      @country.abbreviation_name.should == 'US - United States'
+    end
+    
+    it 'should return the correct string with  params' do
+      @country.abbreviation_name('JJJ').should == 'US - United States JJJ'
+    end
+  end
+
+  context ".abbrev_and_name" do
+    it 'should return the correct string' do
+      @country.abbrev_and_name.should == 'US - United States'
+    end
+  end
 end
