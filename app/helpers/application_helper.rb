4659b57c574b1f8b5e389c6d53ebcda2f5926d4f
Yury Velikanau <yury.spectator@gmail.com>
2010-12-05 12:46:35 -0800

    WIP on new UI based on WebAppTheme.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   ### The next three helpers are great to use to add and remove nested attributes in forms.
   #  LOK AT THIS WEBPAGE FOR REFERENCE
   ## http://openmonkey.com/articles/2009/10/complex-nested-forms-with-rails-unobtrusive-jquery
@@ -10,25 +10,42 @@ EXAMPLE USAGE!!
   <% end %>
   <p><%= add_child_link "New Property", :properties %></p>
   <%= new_child_fields_template(form, :properties, :partial => '/admin/merchandise/add_property')%>
-=end  
+=end
   def remove_child_link(name, f)
     f.hidden_field(:_destroy) + link_to(name, "javascript:void(0)", :class => "remove_child")
   end
-  
+
   def add_child_link(name, association)
     link_to(name, "javascript:void(0);", :class => "add_child", :"data-association" => association)
   end
-  
+
   def new_child_fields_template(form_builder, association, options = {})
     options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
     options[:partial] ||= association.to_s.singularize
     options[:form_builder_local] ||= :f
-    
+
     content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
       form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
         render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
       end
     end
   end
-  
+
+  def commit_button
+    content_tag(:button, :class => :button, :type => :submit) do
+      [
+        image_tag("web-app-theme/icons/tick.png", :alt => "#{t("web-app-theme.save", :default => "Save")}"),
+        t("web-app-theme.save", :default => "Save")
+      ].join.html_safe
+    end
+  end
+
+  def cancel_button(path)
+    name = [
+      image_tag("web-app-theme/icons/cross.png", :alt => t("web-app-theme.cancel", :default => "Cancel")),
+      t("web-app-theme.cancel", :default => "Cancel")
+    ].join(" ").html_safe
+    link_to(name, path, :class => :button)
+  end
+
 end
