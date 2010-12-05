91d700725ad1418270cde3bc0a227d4d338aceb0
Yury Velikanau <yury.spectator@gmail.com>
2010-12-05 12:46:55 -0800

    Minor cleanup of users controller.

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,64 +1,50 @@
 class Admin::UsersController < Admin::BaseController
-  
-  def index 
-   # @users = User.find( :all)
+
+  def index
     authorize! :view_users, current_user
-    
+
     @users = User.admin_grid(params)
-    respond_to do |format|
-      format.html
-      format.json { render :json => @users.to_jqgrid_json(
-        [ :first_name, :last_name, :email, :state ],
-        @users.per_page, #params[:page],
-        @users.current_page, #params[:rows],
-        @users.total_entries)
-      
-      }
-    end
-    
   end
-  
+
   def new
     @user = User.new
     authorize! :create_users, current_user
     form_info
   end
-  
+
   def create
-    
+
     @user = User.new(params[:user])
     authorize! :create_users, current_user
     if @user.save
       @user.deliver_activation_instructions!
-      flash[:notice] = "Your account has been created. Please check your e-mail for your account activation instructions!"
-      redirect_to admin_users_url
+      redirect_to admin_users_url, :notice => "Your account has been created. Please check your e-mail for your account activation instructions!"
     else
       form_info
       render :action => :new
     end
   end
-  
+
   def edit
     @user = User.includes(:roles).find(params[:id])
     authorize! :create_users, current_user
     form_info
   end
-  
+
   def update
     params[:user][:role_ids] ||= []
     @user = User.includes(:roles).find(params[:id])
     authorize! :create_users, current_user
     if @user.update_attributes(params[:user])
-      flash[:notice] = "#{@user.name} has been updated."
-      redirect_to admin_users_url
+      redirect_to admin_users_url, :notice => "#{@user.name} has been updated."
     else
       form_info
       render :action => :edit
     end
   end
-  
+
   private
-  
+
   def form_info
     @all_roles = Role.all
   end
