f5a80d4fc909c8a0d7e47e056e3335d1f2fe2a7f
David Henner <davidhenner@drhenner.local>
2010-12-10 08:29:30 +0000

    Countries form selector should only use countries that you ship to.

--- app/models/country.rb
+++ app/models/country.rb
@@ -1,13 +1,15 @@
 class Country < ActiveRecord::Base
-  
+
   has_many :states
-  
+
   validates :name,  :presence => true
   validates :abbreviation,  :presence => true
-  
+
   USA_ID    = 214
   CANADA_ID = 35
-  
+
+  ACTIVE_COUNTRY_IDS = [CANADA_ID, USA_ID]
+
   def abbreviation_name(append_name = "")
     ([abbreviation, name].join(" - ") + " #{append_name}").strip
   end
@@ -15,12 +17,12 @@ class Country < ActiveRecord::Base
   def abbrev_and_name
     abbreviation_name
   end
-  
+
   # Finds all the countries for a form select .
   #
   # @param none
   # @return [Array] an array of arrays with [string, country.id]
   def self.form_selector
-    find(:all, :order => 'abbreviation ASC').collect { |c| [c.abbrev_and_name, c.id] }
+    find(ACTIVE_COUNTRY_IDS, :order => 'abbreviation ASC').collect { |c| [c.abbrev_and_name, c.id] }
   end
 end
