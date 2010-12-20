515bc5c7baac53ccfc3c6e3e51b275bb258c47c4
David Henner <davidhenner@drhenner.local>
2010-12-20 05:19:43 +0000

    limit 5 best fit products

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,12 +1,12 @@
 class WelcomeController < ApplicationController
-  
+
   def index
     @featured_product = Product.featured
-    @best_selling_products = Product.limit(4)
+    @best_selling_products = Product.limit(5)
     @other_products  ## search 2 or 3 categories (maybe based on the user)
     unless @featured_product
       if current_user && current_user.admin?
-        redirect_to admin_merchandise_products_url 
+        redirect_to admin_merchandise_products_url
       else
         redirect_to login_url
       end
