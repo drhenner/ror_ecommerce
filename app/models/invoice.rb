01cac541bd84a11d83c5aeb02bbb120d41be3b3c
David Henner <davidhenner@drhenner.local>
2010-12-26 13:07:41 +0000

    better yard doc

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -2,24 +2,24 @@ class Invoice < ActiveRecord::Base
   has_many :payments
   has_many :batches, :as => :batchable#, :polymorphic => true
   belongs_to :order
-  
-  
+
+
   validates :amount,        :presence => true
   validates :invoice_type,  :presence => true
   #validates :order_id,      :presence => true
-  
+
   PURCHASE  = 'Purchase'
   RMA       = 'RMA'
-  
+
   INVOICE_TYPES = [PURCHASE, RMA]
   NUMBER_SEED     = 3002001004005
   CHARACTERS_SEED = 20
   #cattr_accessor :gateway
-  
+
   # after_create :create_authorized_transaction
-  
+
   #def create_authorized_transaction
-  #  
+  #
   #end
   state_machine :initial => :pending do
     state :pending
@@ -27,9 +27,9 @@ class Invoice < ActiveRecord::Base
     state :paid
     state :payment_declined
     state :canceled
-    
+
     #after_transition :on => 'cancel', :do => :cancel_authorized_payment
-    
+
     event :payment_rma do
       transition :from => :pending,
                   :to   => :refunded
@@ -52,41 +52,70 @@ class Invoice < ActiveRecord::Base
       transition :from => :authorized,
                   :to   => :authorized
     end
-    
+
     event :cancel do
       transition :from => :authorized,
                   :to  => :canceled
     end
   end
-  
+
+  #  the full shipping address as an array compacted.
+  #
+  # @param [none]
+  # @return [Array]  has ("name", "address1", "address2"(unless nil), "city state zip") or nil
   def order_ship_address_lines
     order.ship_address.try(:full_address_array)
   end
-  
+
+  #  the full order address as an array compacted.
+  #
+  # @param [none]
+  # @return [Array]  has ("name", "address1", "address2"(unless nil), "city state zip") or nil
   def order_billing_address_lines
     order.bill_address.try(:full_address_array)
   end
-  
+
+  # date the invoice was created (not the date it was paid)
+  #
+  # @param [Optional Symbol] date format to be returned
+  # @return [String] formated date
   def invoice_date(format = :us_date)
     I18n.localize(created_at, :format => format)
   end
-  
+
+  # invoice number
+  #
+  # @param [none]
+  # @return [String] invoice number calculated based off the id and preset values
   def number
     (NUMBER_SEED + id).to_s(CHARACTERS_SEED)
   end
-  
+
+  # invoice id calculated from the id of the number
+  #
+  # @param [none]
+  # @return [Integer] invoice id calculated based off the id and preset values
   def self.id_from_number(num)
     num.to_i(CHARACTERS_SEED) - NUMBER_SEED
   end
-  
+
+  # find invoice based off the invoice's number
+  #
+  # @param [String] invoice Number
+  # @return [Invoice] invoice
   def self.find_by_number(num)
     find(id_from_number(num))##  now we can search by id which should be much faster
   end
-  
+
+  # make an invoice object (not saved)
+  #
+  # @param [Integer] order id
+  # @param [Decimal] amount in dollars
+  # @return [Invoice] invoice object
   def Invoice.generate(order_id, charge_amount)
     Invoice.new(:order_id => order_id, :amount => charge_amount, :invoice_type => PURCHASE)
   end
-  
+
   def capture_complete_order
     now = Time.zone.now
     if batches.empty?
@@ -102,18 +131,18 @@ class Invoice < ActiveRecord::Base
     else
       batch       = batches.first
       transaction = CreditCardReceivePayment.new()
-      
+
       debit   = order.user.transaction_ledgers.new(:transaction_account_id => TransactionAccount::CASH_ID,                :debit => amount, :credit => 0,       :period => "#{now.month}-#{now.year}")
       credit  = order.user.transaction_ledgers.new(:transaction_account_id => TransactionAccount::ACCOUNTS_RECEIVABLE_ID, :debit => 0,      :credit => amount,  :period => "#{now.month}-#{now.year}")
-      
+
       transaction.transaction_ledgers.push(credit)
       transaction.transaction_ledgers.push(debit)
-      
+
       batch.transactions.push(transaction)
       batch.save
     end
   end
-  
+
   def authorize_complete_order#(amount)
     x = order.complete!
     now = Time.zone.now
@@ -131,7 +160,7 @@ class Invoice < ActiveRecord::Base
       raise error ###  something messed up I think
     end
   end
-  
+
   def cancel_authorized_payment
     batch       = batches.first
     now = Time.zone.now
@@ -146,7 +175,7 @@ class Invoice < ActiveRecord::Base
       batch.save
     end
   end
-  
+
   def self.process_rma(return_amount, order)
     transaction do
       this_invoice = Invoice.new(:order => order, :amount => return_amount, :invoice_type => RMA)
@@ -156,7 +185,7 @@ class Invoice < ActiveRecord::Base
       this_invoice
     end
   end
-  
+
   def complete_rma_return
     batch       = batches.first || self.batches.create()
     now = Time.zone.now
@@ -168,23 +197,35 @@ class Invoice < ActiveRecord::Base
     batch.transactions.push(transaction)
     batch.save
   end
-  
-  
+
+
+  # call to find the confirmation_id sent by the payment processor.
+  #
+  # @param [none]
+  # @return [String] id the payment processor sends you after authorization.
   def authorization_reference
     if authorization = payments.find_by_action_and_success('authorization', true, :order => 'id ASC')
       authorization.confirmation_id #reference
     end
   end
-  
+
+  # call to find out if the transaction has succeeded.
+  #
+  # @param [none]
+  # @return [Boolean] returns true if the invoice is paid or has been authorized for payment
   def succeeded?
     authorized? || paid?
   end
-  
+
+  # call to find out the amount of the invoice in cents
+  #
+  # @param [none]
+  # @return [Integer] amount of the invoice in cents
   def integer_amount
     times_x_amount = amount.integer? ? 1 : 100
     (amount * times_x_amount).to_i
   end
-  
+
   def authorize_payment(credit_card, options = {})
     options[:number] ||= unique_order_number
     transaction do
@@ -199,7 +240,7 @@ class Invoice < ActiveRecord::Base
       authorization
     end
   end
-  
+
   def capture_payment(options = {})
     transaction do
       capture = Payment.capture(integer_amount, authorization_reference, options)
@@ -213,17 +254,25 @@ class Invoice < ActiveRecord::Base
       capture
     end
   end
-  
+
+  # find the user id of the order associated to the invoice.
+  #
+  # @param [none]
+  # @return [Integer] represents the id of the user
   def user_id
     order.user_id
   end
-  
+
+  # find the user of the order associated to the invoice.
+  #
+  # @param [none]
+  # @return [User]
   def user
     order.user
   end
 
   private
-  
+
   def unique_order_number
     "#{Time.now.to_i}-#{rand(1000000)}"
   end
