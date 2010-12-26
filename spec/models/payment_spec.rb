8fec9d1937b08b9ccf37203528c7c2ff559c62f4
David Henner <davidhenner@drhenner.local>
2010-12-26 21:45:55 +0000

    this method is already tested

--- spec/models/payment_spec.rb
+++ spec/models/payment_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe Payment, " instance methods" do
-  
+
 end
 
 describe Payment, " class methods" do
   before(:each) do
     @amount = 100
   end
-  
+
   context "#store( credit_card, options = {})" do
     it 'should store the credit card' do
       stor = Payment.store(
@@ -28,7 +28,7 @@ describe Payment, " class methods" do
       charge.action.should      == 'unstore'
       charge.message.should     == BogusGateway::SUCCESS_MESSAGE
     end
-    
+
     it 'should not unstore the payment profile' do
       #GATEWAY.expects(:ssl_post).returns(successful_unstore_response)
       charge = Payment.unstore(  '3')
@@ -50,7 +50,7 @@ describe Payment, " class methods" do
       #puts auth.params#[:reference]
       auth[:confirmation_id].should == BogusGateway::AUTHORIZATION
     end
-    
+
     it 'should not authorize the payment with failure' do
       #test failed authorization
       auth = Payment.authorize(
@@ -61,7 +61,7 @@ describe Payment, " class methods" do
       auth.action.should      == 'authorization'
       auth.message.should     == BogusGateway::FAILURE_MESSAGE
     end
-    
+
     it 'should not authorize the payment with error' do
       #test failed authorization
       auth = Payment.authorize(
@@ -87,14 +87,14 @@ describe Payment, " class methods" do
       capt.action.should      == 'capture'
       capt.message.should     == BogusGateway::FAILURE_MESSAGE
     end
-    
+
     it 'should capture the payment in error state' do
       capt = Payment.capture( @amount, '1')
       capt.success.should_not     be_true
       capt.action.should      == 'capture'
       capt.message.should     == BogusGateway::CAPTURE_ERROR_MESSAGE
     end
-    
+
   end
 
   context "#charge( amount, profile_key, options ={})" do
@@ -116,5 +116,5 @@ describe Payment, " class methods" do
     pending "test for validate_card( credit_card, options ={})"
   end
 
-  context "#process(action, amount = nil)" # This method is being exersized by many other methods
+  #context "#process(action, amount = nil)" # This method is being exersized by many other methods
 end
