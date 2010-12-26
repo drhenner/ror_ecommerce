4d77a7919c9865c906a973ab16166a0673dca775
David Henner <davidhenner@drhenner.local>
2010-12-26 21:44:22 +0000

    add test for supplier.admin_grid

--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe Supplier, "#admin_grid(params = {})" do
-  pending "test for admin_grid(params = {})"
+  it "should return Suppliers " do
+    supplier1 = Factory(:supplier)
+    supplier2 = Factory(:supplier)
+    admin_grid = Supplier.admin_grid
+    admin_grid.size.should == 2
+    admin_grid.should == [supplier1, supplier2]
+  end
 end
