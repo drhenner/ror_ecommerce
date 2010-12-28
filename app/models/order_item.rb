5ef664db5811b45db3cc5f762bf9f9cee18c3287
David Henner <davidhenner@drhenner.local>
2010-12-28 07:44:20 +0000

    order items now have docs

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -23,18 +23,36 @@ class OrderItem < ActiveRecord::Base
    #after_transition :to => 'complete', :do => [:update_inventory]
  end
 
+ # if the order item has been shipped it will return true
+ #
+ # @param [none]
+ # @return [Boolean]
   def shipped?
     shipment_id?
   end
 
+  # shipping method for the order item
+  #
+  # @param [none]
+  # @return [ShippingMethod]
   def shipping_method
     shipping_rate.shipping_method
   end
 
+  # shipping method id for the order item (use to reduce SQL calls)
+  #   refer to ShippingMethod ID constants to determine what shipping method is called
+  #
+  # @param [none]
+  # @return [Integer] ShippingMethod id
   def shipping_method_id
     shipping_rate.shipping_method_id
   end
 
+  # called in checkout process. will give you the 'quantity', 'sum of all the prices' and 'sum of all the totals'
+  #  it is better to do the math in SQL than ruby
+  #
+  # @param [Integer]  order.id
+  # @return [OrderItem] Object has addional attributes of [sum_price, sum_total, shipping_category_id, and quantity]
   def self.order_items_in_cart(order_id)
     find(:all, :joins => {:variant => :product },
                :conditions => { :order_items => { :order_id => order_id}},
@@ -45,6 +63,10 @@ class OrderItem < ActiveRecord::Base
                :group => "order_items.variant_id")
   end
 
+  # forces the order to be re-calculated.  If the order item has changed then the order totals need to be adjusted
+  #
+  # @param [none]
+  # @return [none]
   def calculate_order
     if self.ready_to_calculate? &&
         (shipping_rate_id != @beginning_shipping_rate_id || tax_rate_id != @beginning_tax_rate_id)
@@ -53,14 +75,30 @@ class OrderItem < ActiveRecord::Base
     end
   end
 
+  # if something changes to the order item and you dont want to recalculate
+  #   (maybe because you are chnging several more things) then
+  #    this method will mark the calculated at to be nil and thus tell the order that
+  #    it needs to calculate the total again
+  #
+  # @param [none]
+  # @return [none]
   def set_order_calculated_at_to_nil
     order.update_attribute(:calculated_at, nil)
   end
 
+  # determines if the order item has all the attributes set and thus you can now determine the final total
+  #
+  # @param [none]
+  # @return [Boolean]
   def ready_to_calculate?
     shipping_rate_id && tax_rate_id
   end
+
   # this is the price after coupons and anything before calculating the price + tax
+  #  in the future coupons and discounts could change this value
+  #
+  # @param [none]
+  # @return [Float] this is the price that the tax will be applied to.
   def adjusted_price
     ## coupon credit is calculated at the order level but because taxes we need to apply it now
     # => this calculation will be complete in the version of Hadean
@@ -69,12 +107,22 @@ class OrderItem < ActiveRecord::Base
     self.price - coupon_credit
   end
 
+  # this is the price after coupons and taxes
+  #   * this method does not save it just sets the value of total.
+  #   * Thus allowing you to save the whole order with one opperation
+  #
+  # @param [none]
+  # @return [Float] this is the total of the item after taxes and coupons...
   def calculate_total(coupon = nil)
     # shipping charges are calculated in order.rb
 
     self.total = (adjusted_price + tax_charge).round_at(2)
   end
 
+  # the tax charge on an item
+  #
+  # @param [none]
+  # @return [Float] tax charge on the item.
   def tax_charge
     tax_percentage = tax_rate.try(:percentage) ? tax_rate.try(:percentage) : 0.0
     adjusted_price * tax_percentage / 100.0
