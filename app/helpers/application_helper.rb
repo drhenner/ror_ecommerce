f3ca6a45b5de3363779626c77779f36812fec500
David Henner <davidhenner@drhenner.local>
2011-01-03 07:33:21 +0000

    use site_name so developers just need to change one value for the whole site

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  
+
   ### The next three helpers are great to use to add and remove nested attributes in forms.
   #  LOK AT THIS WEBPAGE FOR REFERENCE
   ## http://openmonkey.com/articles/2009/10/complex-nested-forms-with-rails-unobtrusive-jquery
@@ -10,25 +10,29 @@ EXAMPLE USAGE!!
   <% end %>
   <p><%= add_child_link "New Property", :properties %></p>
   <%= new_child_fields_template(form, :properties, :partial => '/admin/merchandise/add_property')%>
-=end  
+=end
+  def site_name
+    "RoR-e.com"
+  end
+
   def remove_child_link(name, f)
     f.hidden_field(:_destroy) + link_to(name, "javascript:void(0)", :class => "remove_child")
   end
-  
+
   def add_child_link(name, association)
     link_to(name, "javascript:void(0);", :class => "add_child", :"data-association" => association)
   end
-  
+
   def new_child_fields_template(form_builder, association, options = {})
     options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
     options[:partial] ||= association.to_s.singularize
     options[:form_builder_local] ||= :f
-    
+
     content_tag(:div, :id => "#{association}_fields_template", :style => "display: none") do
       form_builder.fields_for(association, options[:object], :child_index => "new_#{association}") do |f|
         render(:partial => options[:partial], :locals => {options[:form_builder_local] => f})
       end
     end
   end
-  
+
 end
