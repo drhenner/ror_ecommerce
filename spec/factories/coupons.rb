febe5253562e8ec47ab83ca7431c6497fca59621
David Henner <davidhenner@drhenner.local>
2011-02-07 04:48:17 +0000

    create coupon factory

--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -1,4 +1,22 @@
 # Read about factories at http://github.com/thoughtbot/factory_girl
+#:type          => "CouponValue",
+#:code          => "TEST COUPON",
+#:amount        => "10.00",
+#:minimum_value => "19.99",
+#:percent       =>  nil,
+#:description   =>  "Describe my coupon",
+#:combine       =>  false
+Factory.define :coupon do |f|
+  f.type          "CouponValue"
+  f.code          "TEST COUPON"
+  f.amount        "10.00"
+  f.minimum_value "19.99"
+  f.percent     nil
+  f.description "Describe my coupon"
+  f.combine     false
+  f.starts_at   "2011-01-08 19:39:58"
+  f.expires_at  "2012-01-08 19:39:58"
+end
 
 Factory.define :coupon_percent do |f|
   f.type          "CouponPercent"
@@ -22,4 +40,4 @@ Factory.define :coupon_value do |f|
   f.combine     false
   f.starts_at   "2011-01-08 19:39:58"
   f.expires_at  "2012-01-08 19:39:58"
-end
+end# Read about factories at http://github.com/thoughtbot/factory_girl
