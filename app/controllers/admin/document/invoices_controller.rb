7d99dcdc5b4ceb4f9162b2b7060653f587f476ea
David Henner <davidhenner@drhenner.local>
2011-02-07 04:49:13 +0000

    invoice spec works

--- app/controllers/admin/document/invoices_controller.rb
+++ app/controllers/admin/document/invoices_controller.rb
@@ -1,32 +1,32 @@
-class Admin::Document::InvoicesController < ApplicationController
+class Admin::Document::InvoicesController < Admin::BaseController
   include InvoicePrinter
-  
+
   def index
     @invoices = Invoice.includes([:order]).all
   end
 
   def show
     @invoice = Invoice.includes([:order => [
-                                            :bill_address, 
+                                            :bill_address,
                                             :ship_address
                                             ]]).find(params[:id])
-    
+
     respond_to do |format|
       format.html
       #format.pdf { render :layout => false }
       format.pdf { prawnto :prawn=>{:skip_page_creation=>true}}
     end
   end
-  
+
   def destroy
     @invoice = Invoice.includes([:order]).find(params[:id])
     @invoice.cancel_authorized_payment
     redirect_to admin_document_invoices_url
   end
-  
+
   private
-  
+
   def form_info
-    
+
   end
 end
--- spec/controllers/admin/document/invoices_controller_spec.rb
+++ spec/controllers/admin/document/invoices_controller_spec.rb
@@ -3,11 +3,18 @@ require File.dirname(__FILE__) + '/../../../spec_helper'
 describe Admin::Document::InvoicesController do
   render_views
 
+  before(:each) do
+    activate_authlogic
+
+    @user = Factory(:admin_user)
+    login_as(@user)
+  end
+
   it "index action should render index template" do
     get :index
     response.should render_template(:index)
   end
-  
+
   it "show action should render show template" do
     invoice = Factory(:invoice)
     get :show, :id => invoice.id
