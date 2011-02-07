dbebc34f3a3b928f5d6342172fc0c614fa23179c
David Henner <davidhenner@drhenner.local>
2011-02-07 09:16:14 +0000

    might not be needed any more but assigning birth date was buggy

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,9 +1,9 @@
 class Admin::UsersController < Admin::BaseController
-  
-  def index 
+
+  def index
    # @users = User.find( :all)
     authorize! :view_users, current_user
-    
+
     @users = User.admin_grid(params)
     respond_to do |format|
       format.html
@@ -12,21 +12,22 @@ class Admin::UsersController < Admin::BaseController
         @users.per_page, #params[:page],
         @users.current_page, #params[:rows],
         @users.total_entries)
-      
+
       }
     end
-    
+
   end
-  
+
   def new
     @user = User.new
     authorize! :create_users, current_user
     form_info
   end
-  
+
   def create
-    
+
     @user = User.new(params[:user])
+    @user.format_birth_date(params[:user][:birth_date]) if params[:user][:birth_date].present?
     authorize! :create_users, current_user
     if @user.save
       @user.deliver_activation_instructions!
@@ -37,17 +38,19 @@ class Admin::UsersController < Admin::BaseController
       render :action => :new
     end
   end
-  
+
   def edit
     @user = User.includes(:roles).find(params[:id])
     authorize! :create_users, current_user
     form_info
   end
-  
+
   def update
     params[:user][:role_ids] ||= []
     @user = User.includes(:roles).find(params[:id])
     authorize! :create_users, current_user
+    @user.format_birth_date(params[:user][:birth_date]) if params[:user][:birth_date].present?
+    params[:user].delete_if {|key, value| key.to_s == "birth_date" }
     if @user.update_attributes(params[:user])
       flash[:notice] = "#{@user.name} has been updated."
       redirect_to admin_users_url
@@ -56,9 +59,9 @@ class Admin::UsersController < Admin::BaseController
       render :action => :edit
     end
   end
-  
+
   private
-  
+
   def form_info
     @all_roles = Role.all
   end
