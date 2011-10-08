2fbc4b4979d0c718b2bff46e4df746097e5b2283
David Henner <drhenner@yahoo.com>
2011-10-07 20:04:43 -0500

    remove un-used

--- config/initializers/settings.rb
+++ config/initializers/settings.rb
@@ -1,5 +1,5 @@
-##  THESE SETTING SHOULD NOT NEED TO BE SECURE.  IF YOU NEED TO USE SECURE 
-##  SETTINGS USE -- config/config.yml AND FOLLOW THE INSTRUCTIONS ON THAT PAGE 
+##  THESE SETTING SHOULD NOT NEED TO BE SECURE.  IF YOU NEED TO USE SECURE
+##  SETTINGS USE -- config/config.yml AND FOLLOW THE INSTRUCTIONS ON THAT PAGE
 ##  TO DEPLOY
 
 require 'yaml'
@@ -9,14 +9,14 @@ SETTINGS = {
   :use_foreign_keys => false
 }
 
-APP_CONFIG = YAML.load_file(File.join(Rails.root, "config", "config.yml"))
+#APP_CONFIG = YAML.load_file(File.join(Rails.root, "config", "config.yml"))[Rails.env]
 
 =begin
 ### These options are stored in each of the environment files
 
 #Paperclip.options[:image_magick_path] = '/opt/local/bin'
-if RAILS_ENV == "development" 
-  
+if RAILS_ENV == "development"
+
   Paperclip.options[:command_path] = "DYLD_LIBRARY_PATH='/Users/davidhenner/ImageMagick-6.6.3/lib' /Users/davidhenner/ImageMagick-6.6.3/bin"
 
   #Paperclip.options[:command_path] = '/Users/davidhenner/ImageMagick-6.6.3/bin'
