439d10e19e8d4f40ffba57d5c1cfb0023c41d322
David Henner <drhenner@yahoo.com>
2011-10-07 20:05:16 -0500

    encryption helper needs to used the correct info

--- lib/encryption_helper.rb
+++ lib/encryption_helper.rb
@@ -2,13 +2,13 @@ require 'openssl'
 
 module EncryptionHelper
 
-  #### NOTE #### APP_CONFIG[:encryption_key] from config/config.yml 
+  #### NOTE #### APP_CONFIG[:encryption_key] from config/config.yml
   #   should be stored on the server and not in your applications code base
   module AES256CBC
     def encrypt(plaintext)
       aes = OpenSSL::Cipher::Cipher.new('AES-256-CBC')
       aes.encrypt # must be call before #key and #iv
-      aes.key = APP_CONFIG[:encryption_key]
+      aes.key = HADEAN_CONFIG[:encryption_key]
       aes.iv = iv = aes.random_iv
       ciphertext = aes.update(plaintext)
       ciphertext <<  aes.final
@@ -18,7 +18,7 @@ module EncryptionHelper
     def decrypt(iv, ciphertext)
       aes = OpenSSL::Cipher::Cipher.new('AES-256-CBC')
       aes.decrypt # must be called before aes.key aes.iv
-      aes.key = APP_CONFIG[:encryption_key]
+      aes.key = HADEAN_CONFIG[:encryption_key]
       aes.iv = iv
       plaintext = aes.update(ciphertext)
       plaintext << aes.final
