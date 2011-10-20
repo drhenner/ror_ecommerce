49c2e50bce12527f167996e2889bf918e887c321
David Henner <drhenner@yahoo.com>
2011-10-20 00:49:27 -0400

    better popup dialog

--- public/javascripts/admin/fulfillment.js
+++ public/javascripts/admin/fulfillment.js
@@ -12,34 +12,34 @@ Hadean.Fulfillment = {
   capturePartInvoiceButton  : '#capture-partial-invoice-button-',
   cancelInvoiceButton       : '#cancel-invoice-button-',
   orderId                   : null,
-  
+
   initialize : function(invoiceId, order_id) {
 
     var captureTag      = jQuery(Hadean.Fulfillment.captureInvoiceButton + invoiceId );
     var capturePartTag  = jQuery(Hadean.Fulfillment.capturePartInvoiceButton + invoiceId);
     var cancelTag       = jQuery(Hadean.Fulfillment.cancelInvoiceButton + invoiceId);
     Hadean.Fulfillment.orderId = order_id;
-    
+
     jQuery("#dialog").dialog({
-      bgiframe: true, 
-      autoOpen: false, 
+      bgiframe: true,
+      autoOpen: false,
       height: 190,
       width: 460,
       modal: true
     });
-    
+
     captureTag.
             bind('click',
-              function() { 
+              function() {
                 // submit to collect all payments
                 Hadean.Fulfillment.captureInvoice(invoiceId);
               }
             );
-            
+
 
     capturePartTag.
             bind('click',
-              function() { 
+              function() {
                 // submit to go to capture part form
                 // capture part form has cancel order-items
               }
@@ -47,7 +47,7 @@ Hadean.Fulfillment = {
 
     cancelTag.
             bind('click',
-              function() { 
+              function() {
                 // submit to go to cancel order and payment
 
                 Hadean.Fulfillment.cancelInvoice(invoiceId);
@@ -55,12 +55,12 @@ Hadean.Fulfillment = {
             );
   },//END of INITIALIZE
   captureInvoice : function(invoiceId) {
-    jQuery('#dialog').dialog( 'option', 
-                              'buttons', 
+    jQuery('#dialog').dialog( 'option',
+                              'buttons',
                               [
                                 {
-                                  text: "Ok " + invoiceId,
-                                  click: function() { 
+                                  text: "OK" ,
+                                  click: function() {
                                     // Make an ajax request to cancel the invoice
                                     jQuery.ajax( {
                                       type : "PUT",
@@ -81,24 +81,24 @@ Hadean.Fulfillment = {
                                   }
                                 },
                                 {
-                                  text: "Close Dialog",
+                                  text: "Close",
                                   click: function() { $(this).dialog("close"); }
                                 }
                               ]
                             );
-    jQuery('#dialog-message').html('Are you sure you want to COLLECT FUNDS for this order?');                      
+    jQuery('#dialog-message').html('Are you sure you want to COLLECT FUNDS for this order?');
     jQuery('#dialog-message').css('background-color', '#CFD');
-    jQuery('#dialog').dialog('open'); 
+    jQuery('#dialog').dialog('open');
     return false;
   },// cancelInvoice
   cancelInvoice : function(invoiceId) {
 
-    jQuery('#dialog').dialog( 'option', 
-                              'buttons', 
+    jQuery('#dialog').dialog( 'option',
+                              'buttons',
                               [
                                 {
-                                  text: "Ok " + invoiceId,
-                                  click: function() { 
+                                  text: "OK" ,
+                                  click: function() {
                                     // Make an ajax request to cancel the invoice
                                     jQuery.ajax( {
                                       type : "DELETE",
@@ -107,7 +107,7 @@ Hadean.Fulfillment = {
                                       complete : function(htmlText) {
                                         if (htmlText.status == 200) {
                                           jQuery('#invoice-line-' + invoiceId).html( htmlText.responseText);
-                                          $(this).dialog("close");
+                                          jQuery('#dialog').dialog("close");
                                         } else {
                                           jQuery('#dialog-message').html('Sorry there was an error.');
                                         }
@@ -118,14 +118,14 @@ Hadean.Fulfillment = {
                                   }
                                 },
                                 {
-                                  text: "Close Dialog",
+                                  text: "Close",
                                   click: function() { $(this).dialog("close"); }
                                 }
                               ]
                             );
-    jQuery('#dialog-message').html('Are you sure you want to CANCEL the Order and Shipment?');                      
+    jQuery('#dialog-message').html('Are you sure you want to CANCEL the Order and Shipment?');
     jQuery('#dialog-message').css('background-color', '#FCD');
-    jQuery('#dialog').dialog('open'); 
+    jQuery('#dialog').dialog('open');
     return false;
   }// cancelInvoice
 }
