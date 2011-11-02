60df2f94e781a0b4b8a602f5476e24f183be5a5b
David Henner <drhenner@yahoo.com>
2011-11-01 22:51:56 -0400

    bug + remove un-used actions

--- app/controllers/shopping/addresses_controller.rb
+++ app/controllers/shopping/addresses_controller.rb
@@ -6,31 +6,8 @@ class Shopping::AddressesController < Shopping::BaseController
     form_info
     respond_to do |format|
       format.html # index.html.erb
-      format.xml  { render :xml => @shopping_addresses }
     end
   end
-
-  # GET /shopping/addresses/1
-  # GET /shopping/addresses/1.xml
-#  def show
-#    @shopping_address = Address.find(params[:id])
-#
-#    respond_to do |format|
-#      format.html # show.html.erb
-#      format.xml  { render :xml => @shopping_address }
-#    end
-#  end
-
-  # GET /shopping/addresses/new
-  # GET /shopping/addresses/new.xml
-#  def new
-#    @shopping_address = Address.new
-#
-#    respond_to do |format|
-#      format.html # new.html.erb
-#      format.xml  { render :xml => @shopping_address }
-#    end
-#  end
 
   # GET /shopping/addresses/1/edit
   def edit
@@ -46,18 +23,16 @@ class Shopping::AddressesController < Shopping::BaseController
       @shopping_address.billing_default = true  if current_user.default_billing_address.nil?
       @shopping_address.save
     elsif params[:shopping_address_id].present?
-      @shopping_address = Address.find(params[:shopping_address_id])
+      @shopping_address = current_user.addresses.find(params[:shopping_address_id])
     end
     respond_to do |format|
-      
+
       if @shopping_address.id
         update_order_address_id(@shopping_address.id)
         format.html { redirect_to(shopping_orders_url, :notice => 'Address was successfully created.') }
-        format.xml  { render :xml => @shopping_address, :status => :created, :location => @shopping_address }
       else
         form_info
         format.html { render :action => "index" }
-        format.xml  { render :xml => @shopping_address.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -68,22 +43,20 @@ class Shopping::AddressesController < Shopping::BaseController
     #old_address = current_user.addresses.find(params[:id])
     #Address.update_address(old_address, params[:shopping_address], AddressType::SHIPPING_ID, find_or_create_order )
     @shopping_address = current_user.addresses.new(params[:address])
-    
+
     # if we are editing the current default address then this is the default address
     @shopping_address.default = true if params[:id] == current_user.default_shipping_address.try(:id)
-    
+
     respond_to do |format|
-      if @shopping_address.valid? && @shopping_address.save_default_address
+      if @shopping_address.valid? && @shopping_address.save_default_address(current_user, params[:address])
         old_shopping_address = current_user.addresses.find(params[:id])
         old_shopping_address.update_attributes(:active => false)
         update_order_address_id(@shopping_address.id)
         format.html { redirect_to(shopping_orders_url, :notice => 'Address was successfully updated.') }
-        format.xml  { head :ok }
       else
         @shopping_address.update_attributes(params[:address])
         @states     = State.form_selector
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @shopping_address.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -103,20 +76,19 @@ class Shopping::AddressesController < Shopping::BaseController
 
     respond_to do |format|
       format.html { redirect_to(shopping_addresses_url) }
-      format.xml  { head :ok }
     end
   end
-  
+
   private
-  
+
   def form_info
     @shopping_addresses = current_user.shipping_addresses
     @states     = State.form_selector
   end
-  
+
   def update_order_address_id(id)
     session_order.update_attributes(
-                          :ship_address_id => id , 
+                          :ship_address_id => id ,
                           :bill_address_id => (session_order.bill_address_id ? session_order.bill_address_id : id)
                                     )
   end
