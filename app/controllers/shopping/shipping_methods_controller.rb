8e520543e59d99a05a80ba781aa6e2739a286c1f
David Henner <drhenner@yahoo.com>
2011-11-01 22:51:25 -0400

    remove un-used actions

--- app/controllers/shopping/shipping_methods_controller.rb
+++ app/controllers/shopping/shipping_methods_controller.rb
@@ -21,38 +21,6 @@ class Shopping::ShippingMethodsController < Shopping::BaseController
     end
   end
 
-  # GET /shopping/shipping_methods/1
-  # GET /shopping/shipping_methods/1.xml
-  def show
-    @shipping_method = ShippingMethod.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @shipping_method }
-    end
-  end
-
-  # GET /shopping/shipping_methods/1/edit
-  def edit
-    @shipping_method = ShippingMethod.find(params[:id])
-  end
-
-  # POST /shopping/shipping_methods
-  # POST /shopping/shipping_methods.xml
-  def create
-    @shipping_method = ShippingMethod.new(params[:shopping_shipping_method])
-
-    respond_to do |format|
-      if @shipping_method.save
-        format.html { redirect_to(@shipping_method, :notice => I18n.t('shipping_method_created')) }
-        format.xml  { render :xml => @shipping_method, :status => :created, :location => @shipping_method }
-      else
-        format.html { render :action => "new" }
-        format.xml  { render :xml => @shipping_method.errors, :status => :unprocessable_entity }
-      end
-    end
-  end
-
   # PUT /shopping/shipping_methods/1
   # PUT /shopping/shipping_methods/1.xml
   def update
@@ -78,15 +46,4 @@ class Shopping::ShippingMethodsController < Shopping::BaseController
     end
   end
 
-  # DELETE /shopping/shipping_methods/1
-  # DELETE /shopping/shipping_methods/1.xml
-  def destroy
-    @shipping_method = ShippingMethod.find(params[:id])
-    @shipping_method.destroy
-
-    respond_to do |format|
-      format.html { redirect_to(shopping_shipping_methods_url) }
-      format.xml  { head :ok }
-    end
-  end
 end
