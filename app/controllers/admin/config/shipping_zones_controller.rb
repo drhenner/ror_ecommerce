748d1696224b59db074ebb27fd273f3bc3b66409
David Henner <drhenner@yahoo.com>
2011-11-13 13:45:17 -0500

    shipping zone tests

--- app/controllers/admin/config/shipping_zones_controller.rb
+++ app/controllers/admin/config/shipping_zones_controller.rb
@@ -3,33 +3,18 @@ class Admin::Config::ShippingZonesController < Admin::Config::BaseController
   # GET /admin/config/shipping_zones.xml
   def index
     @shipping_zones = ShippingZone.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.xml  { render :xml => @shipping_zones }
-    end
   end
 
   # GET /admin/config/shipping_zones/1
   # GET /admin/config/shipping_zones/1.xml
   def show
     @shipping_zone = ShippingZone.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.xml  { render :xml => @shipping_zone }
-    end
   end
 
   # GET /admin/config/shipping_zones/new
   # GET /admin/config/shipping_zones/new.xml
   def new
     @shipping_zone = ShippingZone.new
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.xml  { render :xml => @shipping_zone }
-    end
   end
 
   # GET /admin/config/shipping_zones/1/edit
@@ -45,10 +30,8 @@ class Admin::Config::ShippingZonesController < Admin::Config::BaseController
     respond_to do |format|
       if @shipping_zone.save
         format.html { redirect_to(admin_config_shipping_zone_url(@shipping_zone), :notice => 'Shipping zone was successfully created.') }
-        format.xml  { render :xml => @shipping_zone, :status => :created, :location => @shipping_zone }
       else
         format.html { render :action => "new" }
-        format.xml  { render :xml => @shipping_zone.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -61,10 +44,8 @@ class Admin::Config::ShippingZonesController < Admin::Config::BaseController
     respond_to do |format|
       if @shipping_zone.update_attributes(params[:shipping_zone])
         format.html { redirect_to(admin_config_shipping_zone_url(@shipping_zone), :notice => 'Shipping zone was successfully updated.') }
-        format.xml  { head :ok }
       else
         format.html { render :action => "edit" }
-        format.xml  { render :xml => @shipping_zone.errors, :status => :unprocessable_entity }
       end
     end
   end
@@ -77,7 +58,6 @@ class Admin::Config::ShippingZonesController < Admin::Config::BaseController
 
     respond_to do |format|
       format.html { redirect_to(admin_config_shipping_zones_url) }
-      format.xml  { head :ok }
     end
   end
 end
---
+++ spec/controllers/admin/config/shipping_zones_controller_spec.rb
@@ -0,0 +1,67 @@
+require  'spec_helper'
+
+describe Admin::Config::ShippingZonesController do
+  render_views
+
+  before(:each) do
+    activate_authlogic
+
+    @user = Factory(:admin_user)
+    login_as(@user)
+  end
+
+  it "index action should render index template" do
+    get :index
+    response.should render_template(:index)
+  end
+
+  it "show action should render show template" do
+    @shipping_zone = ShippingZone.first
+    get :show, :id => @shipping_zone.id
+    response.should render_template(:show)
+  end
+
+  it "new action should render new template" do
+    get :new
+    response.should render_template(:new)
+  end
+
+  it "create action should render new template when model is invalid" do
+    ShippingZone.any_instance.stubs(:valid?).returns(false)
+    post :create
+    response.should render_template(:new)
+  end
+
+  it "create action should redirect when model is valid" do
+    ShippingZone.any_instance.stubs(:valid?).returns(true)
+    post :create, :shipping_zone => {:name => 'Alaska'}
+    response.should redirect_to(admin_config_shipping_zone_url(assigns[:shipping_zone]))
+  end
+
+  it "edit action should render edit template" do
+    @shipping_zone = ShippingZone.first
+    get :edit, :id => @shipping_zone.id
+    response.should render_template(:edit)
+  end
+
+  it "update action should render edit template when model is invalid" do
+    @shipping_zone = ShippingZone.first
+    ShippingZone.any_instance.stubs(:valid?).returns(false)
+    put :update, :id => @shipping_zone.id
+    response.should render_template(:edit)
+  end
+
+  it "update action should redirect when model is valid" do
+    @shipping_zone = ShippingZone.first
+    ShippingZone.any_instance.stubs(:valid?).returns(true)
+    put :update, :id => @shipping_zone.id
+    response.should redirect_to(admin_config_shipping_zone_url(assigns[:shipping_zone]))
+  end
+
+  it "destroy action should destroy model and redirect to index action" do
+    @shipping_zone = ShippingZone.first
+    delete :destroy, :id => @shipping_zone.id
+    response.should redirect_to(admin_config_shipping_zones_url)
+    ShippingZone.exists?(@shipping_zone.id).should be_false
+  end
+end
