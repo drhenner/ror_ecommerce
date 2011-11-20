1cd5ec1c68aded3f4289b0774d3dbc222d5809b3
David Henner <drhenner@yahoo.com>
2011-11-19 23:05:02 -0500

    add test for product controller

---
+++ spec/controllers/products_controller_spec.rb
@@ -0,0 +1,20 @@
+require File.dirname(__FILE__) + '/../spec_helper'
+
+describe ProductsController do
+
+  before(:each) do
+    @product = Factory(:product)
+    @variant = Factory(:variant, :product => @product)
+    @variant.stubs(:primary_property).returns(nil)
+  end
+
+  it "index action should render index template" do
+    get :index
+    response.should render_template(:index)
+  end
+
+  it "show action should not blow up without a property association" do
+    get :show, :id => @product.permalink
+    response.should render_template(:show)
+  end
+end
