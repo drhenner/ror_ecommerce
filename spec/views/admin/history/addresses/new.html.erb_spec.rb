99b362844a4d337274f98b08bb3987c9fdd6ef50
David Henner <drhenner@yahoo.com>
2011-11-19 21:39:08 -0500

    stub helper method for view spec

--- spec/views/admin/history/addresses/new.html.erb_spec.rb
+++ spec/views/admin/history/addresses/new.html.erb_spec.rb
@@ -4,6 +4,7 @@ describe "admin/history/addresses/new.html.erb" do
   before(:each) do
     @order = Factory(:order)
     @address = Factory.build(:address)
+    view.stubs(:states).returns([])
   end
 
   it "renders new address form" do
