b33ad81814fdc51f6931b6855acf55906aa5b2da
David Henner <drhenner@yahoo.com>
2011-11-24 06:19:58 -0500

    admin property controller test

--- app/controllers/admin/merchandise/properties_controller.rb
+++ app/controllers/admin/merchandise/properties_controller.rb
@@ -9,23 +9,16 @@ class Admin::Merchandise::PropertiesController < Admin::BaseController
         @properties.per_page, #params[:page],
         @properties.current_page, #params[:rows],
         @properties.total_entries)
-        
       }
     end
   end
-  
-  def show
-    @property = Property.find(params[:id])
-    respond_with(@property)
-  end
-  
+
   def new
     @property = Property.new
   end
-  
+
   def create
     @property = Property.new(params[:property])
-    
     if @property.save
       redirect_to :action => :index
     else
@@ -33,26 +26,25 @@ class Admin::Merchandise::PropertiesController < Admin::BaseController
       render :action => :new
     end
   end
-  
+
   def edit
     @property = Property.find(params[:id])
   end
-  
+
   def update
     @property = Property.find(params[:id])
-    
     if @property.update_attributes(params[:property])
       redirect_to :action => :index
     else
       render :action => :edit
     end
   end
-  
+
   def destroy
     @property = Property.find(params[:id])
     @property.active = false
     @property.save
-    
+
     redirect_to :action => :index
   end
 
---
+++ spec/controllers/admin/merchandise/properties_controller_spec.rb
@@ -0,0 +1,63 @@
+require  'spec_helper'
+
+describe Admin::Merchandise::PropertiesController do
+  render_views
+
+  before(:each) do
+    activate_authlogic
+
+    @user = Factory(:admin_user)
+    login_as(@user)
+
+    controller.stubs(:current_ability).returns(Ability.new(@user))
+  end
+
+  it "index action should render index template" do
+    get :index
+    response.should render_template(:index)
+  end
+
+  it "new action should render new template" do
+    get :new
+    response.should render_template(:new)
+  end
+
+  it "create action should render new template when model is invalid" do
+    Property.any_instance.stubs(:valid?).returns(false)
+    post :create
+    response.should render_template(:new)
+  end
+
+  it "create action should redirect when model is valid" do
+    Property.any_instance.stubs(:valid?).returns(true)
+    post :create, :property => {:display_name => 'dis', :identifing_name => 'test'}
+    response.should redirect_to(admin_merchandise_properties_url)
+  end
+
+  it "edit action should render edit template" do
+    @property = Factory(:property)
+    get :edit, :id => @property.id
+    response.should render_template(:edit)
+  end
+
+  it "update action should render edit template when model is invalid" do
+    @property = Factory(:property)
+    Property.any_instance.stubs(:valid?).returns(false)
+    put :update, :id => @property.id
+    response.should render_template(:edit)
+  end
+
+  it "update action should redirect when model is valid" do
+    @property = Factory(:property)
+    Property.any_instance.stubs(:valid?).returns(true)
+    put :update, :id => @property.id
+    response.should redirect_to(admin_merchandise_properties_url)
+  end
+
+  it "destroy action should destroy model and redirect to index action" do
+    @property = Factory(:property)
+    delete :destroy, :id => @property.id
+    response.should redirect_to(admin_merchandise_properties_url)
+    Property.find(@property.id).active.should be_false
+  end
+end
