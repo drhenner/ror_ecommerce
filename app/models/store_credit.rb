5551cafc4042d0f498d3c896c191c2f5b89bd29c
David Henner <drhenner@yahoo.com>
2011-11-24 00:49:11 -0500

    fix for bad SQL math with decimal (sqlite issue only)

--- app/models/store_credit.rb
+++ app/models/store_credit.rb
@@ -6,12 +6,19 @@ class StoreCredit < ActiveRecord::Base
   validates :user_id, :presence => true
   validates :amount , :presence => true
 
+  after_find :ensure_sql_math_rounding_issues
   # removes amount from object using SQL math
   #
   # @param [Float] amount to remove
   # @return [none]
   def remove_credit(amount_to_remove)
-    sql = "UPDATE store_credits SET amount = (amount - #{amount_to_remove}) WHERE id = #{self.id}"
+    sql = "UPDATE store_credits SET amount = (amount - #{amount_to_remove.to_f.round_at(2)}) WHERE id = #{self.id}"
     ActiveRecord::Base.connection.execute(sql)
+  end
+
+  private
+
+  def ensure_sql_math_rounding_issues
+    self.amount = amount.to_f.round_at(2)
   end
 end
