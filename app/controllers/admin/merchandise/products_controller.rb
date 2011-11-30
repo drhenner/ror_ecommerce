acf02244e66f51169767baf049f0bfcf8f16033f
David Henner <drhenner@yahoo.com>
2011-11-30 18:26:48 -0500

    no need for product types  anymore

--- app/controllers/admin/merchandise/products_controller.rb
+++ app/controllers/admin/merchandise/products_controller.rb
@@ -108,7 +108,7 @@ class Admin::Merchandise::ProductsController < Admin::BaseController
     def form_info
       @prototypes               = Prototype.all.collect{|pt| [pt.name, pt.id]}
       @all_properties           = Property.all
-      @select_product_types     = ProductType.all.collect{|pt| [pt.name, pt.id]}
+      #@select_product_types     = ProductType.all#.collect{|pt| [pt.name, pt.id]}
       #@all_shipping_rates = ShippingRate.all#.collect {|sr| [sr.name, sr.id]}
       @select_shipping_category = ShippingCategory.all.collect {|sc| [sc.name, sc.id]}
       @select_tax_status        = TaxStatus.all.collect {|ts| [ts.name, ts.id]}
