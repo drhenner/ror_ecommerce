aed942fb1eedb008ac0f5c2904c970d5021edb2f
David Henner <drhenner@yahoo.com>
2012-01-21 00:40:53 -0500

    refactor tests for new implementation

--- spec/models/product_type_spec.rb
+++ spec/models/product_type_spec.rb
@@ -5,8 +5,9 @@ describe ProductType, '#admin_grid(params = {})' do
     product_type1 = Factory(:product_type)
     product_type2 = Factory(:product_type)
     admin_grid = ProductType.admin_grid
-    admin_grid.size.should == 2
-    admin_grid.include?(product_type1).should be_true
-    admin_grid.include?(product_type2).should be_true
+    info = admin_grid.all
+    info.size.should == 2
+    info.include?(product_type1).should be_true
+    info.include?(product_type2).should be_true
   end
 end
--- spec/models/supplier_spec.rb
+++ spec/models/supplier_spec.rb
@@ -5,7 +5,8 @@ describe Supplier, "#admin_grid(params = {})" do
     supplier1 = Factory(:supplier)
     supplier2 = Factory(:supplier)
     admin_grid = Supplier.admin_grid
-    admin_grid.size.should == 2
-    admin_grid.should == [supplier1, supplier2]
+    info =admin_grid.all
+    info.size.should == 2
+    info.should == [supplier1, supplier2]
   end
 end
