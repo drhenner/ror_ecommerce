f4e3d5e683672ccde07206e0de4f5dd57bfbdecc
David Henner <drhenner@yahoo.com>
2012-01-25 01:13:01 -0500

    add photo from an http URL

--- app/models/product.rb
+++ app/models/product.rb
@@ -63,7 +63,7 @@ class Product < ActiveRecord::Base
   accepts_nested_attributes_for :variants, :reject_if => proc { |attributes| attributes['sku'].blank? }
   accepts_nested_attributes_for :product_properties, :reject_if => proc { |attributes| attributes['description'].blank? }, :allow_destroy => true
 
-  accepts_nested_attributes_for :images, :reject_if => lambda { |t| t['photo'].nil? }
+  accepts_nested_attributes_for :images, :reject_if => lambda { |t| (t['photo'].nil? && t['photo_from_link'].blank?) }
 
   validates :shipping_category_id,  :presence => true
   validates :tax_status_id,         :presence => true
