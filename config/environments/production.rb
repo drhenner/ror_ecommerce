5c725a545f90dab90317ac7cc100017e993b1414
David Henner <drhenner@yahoo.com>
2012-02-15 11:49:45 -0500

    default host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,6 +76,7 @@ Hadean::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
+  config.action_mailer.default_url_options = { :host => 'rore.example.com' }
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
