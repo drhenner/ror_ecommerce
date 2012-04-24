a268827b155c2f1036c0402a3af48b37b2113b88
David Henner <drhenner@yahoo.com>
2012-04-24 17:35:07 -0400

    if the order doesn't have a user then the first purchase discount is false

--- app/models/coupon_first_purchase.rb
+++ app/models/coupon_first_purchase.rb
@@ -1,11 +1,11 @@
 class CouponFirstPurchaseValue < CouponValue
   def eligible?(order, at = Time.zone.now )
-    (starts_at <= at && expires_at >= at) && order.user.try(:number_of_finished_orders_at, at)
+    (starts_at <= at && expires_at >= at) && (order.user.try(:number_of_finished_orders_at, at) == 0) rescue false
   end
 end
 
 class CouponFirstPurchasePercent < CouponPercent
   def eligible?(order, at = Time.zone.now)
-    (starts_at <= at && expires_at >= at) && order.user.try(:number_of_finished_orders_at, at)
+    (starts_at <= at && expires_at >= at) && (order.user.try(:number_of_finished_orders_at, at) == 0) rescue false
   end
 end
