da23e0da28108c324fb569a43e67d0cd35aea1dd
Denis Peplin <denis.peplin@gmail.com>
2012-05-30 13:17:16 +0400

    Catch DeleteRestrictionError

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,6 +25,10 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  rescue_from ActiveRecord::DeleteRestrictionError do |exception|
+    redirect_to :back, alert: exception.message
+  end
+
   def current_ability
     @current_ability ||= Ability.new(current_user)
   end
