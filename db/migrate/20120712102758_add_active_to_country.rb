29542158db17df10df66e074628d05c1406d7a6f
drhenner <drhenner@gmail.com>
2012-07-18 11:52:35 -0400

    better migration

--- db/migrate/20120712102758_add_active_to_country.rb
+++ db/migrate/20120712102758_add_active_to_country.rb
@@ -2,12 +2,12 @@ class AddActiveToCountry < ActiveRecord::Migration
   def change
     add_column :countries, :active, :boolean, :null => false, :default => false
     Country.reset_column_information
-    usa = Country.find_by_id(214)
+    usa = Country.find_by_id(Country::USA_ID)
     if usa # this means they are upgrading their local copy
       usa.active = true
       usa.save # lets make USA active so people don't have to configure to see things working
     end
-    canada = Country.find_by_id(35)
+    canada = Country.find_by_id(Country::CANADA_ID)
     if canada # this means they are upgrading their local copy
       canada.active = true
       canada.save # lets make CANADA active so people don't have to configure to see things working
