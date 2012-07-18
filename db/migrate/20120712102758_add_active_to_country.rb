e2f107dbe51315db271e42836f55fa64ea769b9a
drhenner <drhenner@gmail.com>
2012-07-18 11:45:03 -0400

    lets make USA active so people don't have to configure to see things working

--- db/migrate/20120712102758_add_active_to_country.rb
+++ db/migrate/20120712102758_add_active_to_country.rb
@@ -1,5 +1,17 @@
 class AddActiveToCountry < ActiveRecord::Migration
   def change
     add_column :countries, :active, :boolean, :null => false, :default => false
+    Country.reset_column_information
+    usa = Country.find_by_id(214)
+    if usa # this means they are upgrading their local copy
+      usa.active = true
+      usa.save # lets make USA active so people don't have to configure to see things working
+    end
+    canada = Country.find_by_id(35)
+    if canada # this means they are upgrading their local copy
+      canada.active = true
+      canada.save # lets make CANADA active so people don't have to configure to see things working
+    end
+    add_index  :addresses, :active  # we will search for active almost every time.  normally I don't do this for a boolean
   end
 end
