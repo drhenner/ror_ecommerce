ef24739efd8c52f9a0db47b8c313afe052e02074
David Henner <drhenner@yahoo.com>
2012-09-08 12:39:19 -0700

    better look and feel / navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -30,6 +30,10 @@ EXAMPLE USAGE!!
   def add_child_link(name, association)
     link_to(name, "javascript:void(0);", :class => "add_child", :"data-association" => association)
   end
+  
+  def add_child_button(name, association)
+    link_to(name, "javascript:void(0);", :class => "add_child button", :"data-association" => association)
+  end
 
   def new_child_fields_template(form_builder, association, options = {})
     options[:object] ||= form_builder.object.class.reflect_on_association(association).klass.new
