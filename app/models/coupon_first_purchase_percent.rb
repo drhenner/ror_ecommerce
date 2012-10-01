b53ab357ee2b932186a4100c69892004fccf3c8c
David Henner <drhenner@yahoo.com>
2012-10-01 13:33:31 -0700

    coupons update

---
+++ app/models/coupon_first_purchase_percent.rb
@@ -0,0 +1,6 @@
+class CouponFirstPurchasePercent < CouponPercent
+  def eligible?(order, at = nil)
+    at ||= order.completed_at || Time.zone.now
+    (starts_at <= at && expires_at >= at) && (order.user.try(:number_of_finished_orders_at, at) == 0) rescue false
+  end
+end
--- spec/factories/coupons.rb
+++ spec/factories/coupons.rb
@@ -19,8 +19,7 @@ FactoryGirl.define do
     expires_at  "2012-01-08 19:39:58"
   end
 
-  factory :coupon_percent do
-    type          "CouponPercent"
+  factory :coupon_percent, class: "CouponPercent" do
     code          "TEST COUPON"
     amount        "10.00"
     minimum_value "19.99"
@@ -31,8 +30,7 @@ FactoryGirl.define do
     expires_at  "2012-01-08 19:39:58"
   end
 
-  factory :coupon_value do
-    type          "CouponValue"
+  factory :coupon_value, class: "CouponValue" do
     code          "TEST COUPON"
     amount        "10.00"
     minimum_value "19.99"
@@ -41,5 +39,10 @@ FactoryGirl.define do
     combine     false
     starts_at   "2011-01-08 19:39:58"
     expires_at  "2012-01-08 19:39:58"
+  end
+  factory :coupon_one_time_percent, class: "CouponOneTimePercent" do
+    percent 10.0
+  end
+  factory :coupon_one_time_value, class: "CouponOneTimeValue" do
   end
 end# Read about factories at http://github.com/thoughtbot/factory_girl
