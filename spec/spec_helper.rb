e76a3ce3c46667210ea0aa576611c088f2d55ce6
David Henner <drhenner@yahoo.com>
2012-10-09 09:00:39 -0700

    update capybara

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,7 +7,7 @@ require "authlogic/test_case"
 #require 'shoulda'
 require 'mocha'
 require "email_spec"
-require 'capybara/rspec'
+#require 'capybara/rspec'
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
