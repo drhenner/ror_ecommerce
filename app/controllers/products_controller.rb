fe6c58334e8230a6c63f45291877003d3d7ed3b4
David Henner <drhenner@yahoo.com>
2013-01-08 09:20:26 -0800

    products page query all for no params

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,13 +2,16 @@ class ProductsController < ApplicationController
 
   def index
     products = Product.active.includes(:variants)
-    
+
     product_types = nil
     if params[:product_type_id].present? && product_type = ProductType.find_by_id(params[:product_type_id])
       product_types = product_type.self_and_descendants.map(&:id)
     end
-
-    @products = products.where('product_type_id IN (?)', product_types || featured_product_types)
+    if product_types
+      @products = products.where('product_type_id IN (?)', product_types)
+    else
+      @products = products.all
+    end
   end
 
   def create
