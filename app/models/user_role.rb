3ad71034eec6336aec7aa1e29189ed731fbbb3ba
David Henner <drhenner@yahoo.com>
2013-01-10 12:36:59 -0800

    prepare for rails 3

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,7 +1,8 @@
 class UserRole < ActiveRecord::Base
   belongs_to :user
   belongs_to :role
-  
-  validates :user_id,  :presence => true
+
+ # validates :user_id,  :presence => true
   validates :role_id,  :presence => true
+
 end
