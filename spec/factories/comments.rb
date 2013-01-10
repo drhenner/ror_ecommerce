5b0c09e3b59aa26dc576bca7eecae9399dd742d5
David Henner <drhenner@yahoo.com>
2013-01-09 23:56:41 -0800

    fix for rails 3.2.11

--- spec/factories/comments.rb
+++ spec/factories/comments.rb
@@ -2,7 +2,7 @@ FactoryGirl.define do
   factory :comment do
     note "My Note"
     commentable { |c| c.association(:return_authorization) }
-    created_by  { |c| c.association(:user) }
+    created_by  { |c| c.association(:user).id }
     user        { |c| c.association(:user) }
   end
 end
