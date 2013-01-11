d413505f9247a76d5634d6ae66ec53515f3625c8
David Henner <drhenner@yahoo.com>
2013-01-11 00:37:51 -0800

    phone factory works now

--- spec/factories/phone.rb
+++ spec/factories/phone.rb
@@ -1,8 +1,8 @@
 FactoryGirl.define do
   factory :phone do
-    phone_type_id   { PhoneType.first }
+    phone_type   { PhoneType.first }
     phoneable_type       'User'
-    phoneable_id       { |c| c.association(:user).id }
+    phoneable_id       { FactoryGirl.create(:user).id }
     number          '919-636-0383'
     primary         true
   end
