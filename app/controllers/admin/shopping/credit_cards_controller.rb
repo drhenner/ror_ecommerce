c81ef214204dbc04a2b53c40ce28fb01bec9963c
David Henner <drhenner@yahoo.com>
2013-02-02 23:35:29 -0800

    remove un-needed scaffold code

--- app/controllers/admin/shopping/credit_cards_controller.rb
+++ app/controllers/admin/shopping/credit_cards_controller.rb
@@ -1,36 +1,20 @@
 class Admin::Shopping::CreditCardsController < Admin::Shopping::BaseController
   # GET /admin/order/credit_cards
-  # GET /admin/order/credit_cards.xml
   def index
     @credit_cards = session_admin_cart[:user].credit_cards.all
-
-    respond_to do |format|
-      format.html # index.html.erb
-    end
   end
 
   # GET /admin/order/credit_cards/1
-  # GET /admin/order/credit_cards/1.xml
   def show
     @credit_card = session_admin_cart[:user].credit_cards.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-    end
   end
 
   # GET /admin/order/credit_cards/new
-  # GET /admin/order/credit_cards/new.xml
   def new
     @credit_card = CreditCard.new(:user_id => session_admin_cart[:user].id)
-
-    respond_to do |format|
-      format.html # new.html.erb
-    end
   end
 
   # POST /admin/order/credit_cards
-  # POST /admin/order/credit_cards.xml
   def create
     @credit_card = session_admin_cart[:user].credit_cards.new(params[:credit_card])
 
@@ -42,6 +26,4 @@ class Admin::Shopping::CreditCardsController < Admin::Shopping::BaseController
       end
     end
   end
-
-
 end
