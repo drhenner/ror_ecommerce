fdeed7a6de7be4f09bae1b279d02c3be329a0023
David Henner <drhenner@yahoo.com>
2013-02-04 13:24:08 -0800

    add some docs for cart in a more organized way

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,8 +1,15 @@
-# the cart is simply a bucket of stuff.  it is very well documented in a blog post I have posted below and edited slightly for trumaker:
+# the cart is simply a bucket of stuff.  it is very well documented in a blog post I have posted:
 
-# The first thing that comes to mind of a great cart is that it is "stupid". Sure that might sound crazy but it is very true. A shopping cart should not know much about your "order" or your "products". Your cart should simply be a bucket to keep a bunch of products for a specific user. It doesn't know the price of the products or anything about the checkout process.
+# The first thing that comes to mind of a great cart is that it is "stupid".
+# Sure that might sound crazy but it is very true. A shopping cart should not know much
+# about your "order" or your "products". Your cart should simply be a bucket to keep a
+# bunch of products for a specific user. It doesn't know the price of the products or
+# anything about the checkout process.
 #
-# The second thing about a great cart is that it ironically it is "smart" about itself. Your cart should NOT know about the order or products but it should know about itself. Note your cart is the combination of a "cart model" and "cart_items model". This cart_items model has the following fields:
+# The second thing about a great cart is that it ironically it is "smart" about itself.
+# Your cart should NOT know about the order or products but it should know about itself.
+# Note your cart is the combination of a "cart model" and "cart_items model". This cart_items
+# model has the following fields:
 #
 # cart_id
 # product_id
@@ -16,7 +23,8 @@
 #
 # Also note I am in favor of adding user_id to the cart_items table to keep queries more simple.
 #
-# One feature I added to the minimum model is the field item_type_id. item_type_id refers to a model that just has a name field. The ItemTypes are as follows:
+# One feature I added to the minimum model is the field item_type_id. item_type_id refers to a
+# model that just has a name field. The ItemTypes are as follows:
 #
 # shopping_cart
 # save_for_later
@@ -24,32 +32,48 @@
 # purchased
 # deleted
 # admin_cart
-# What this simple field in the DB does is allows you to have a wish list and save for later functionality out of the box. Additionally you can WOW your marketing team by telling them all the items a user has ever deleted out of their cart or purchased. If you ever need to create a recommendation engine your cart will give you all the data you need.
 #
-# Now with this model, purchasing an item is simply taking the items that are in the cart and have a item_type of shopping_cart and moving them to an order / order_items object.  Once you purchase an order_item you change the cart_item.item_type to "purchased" with cart.mark_items_purchased(order)
+# What this simple field in the DB does is allows you to have a wish list and save for later
+# functionality out of the box. Additionally you can WOW your marketing team by telling them
+# all the items a user has ever deleted out of their cart or purchased. If you ever need to create
+# a recommendation engine your cart will give you all the data you need.
+#
+# Now with this model, purchasing an item is simply taking the items that are in the cart and
+# have an item_type of shopping_cart and moving them to an order / order_items object.  Once
+# you purchase an order_item you change the cart_item.item_type to "purchased" with cart.mark_items_purchased(order)
 
 # _______________________
 
 
-# Combining the Cart and Order Objects
+# == Combining the Cart and Order Objects
 #
-# I've heard the argument that using an order object for the cart "make things easier". Not only do I disagree but sorry, "You would be wrong". By mixing the cart and the order you have not separated concerns. This can make validations very conditional. It also mixes cart logic with order logic.
+# I've heard the argument that using an order object for the cart "make things easier".
+# Not only do I disagree but sorry, "You would be wrong". By mixing the cart and the order you have not
+# separated concerns. This can make validations very conditional. It also mixes cart logic with order logic.
 #
-# I view your cart as something that can be removed off the face of the planet and not effect much. Sure people would be upset to add things back to their cart but at the end of the day it would not effect anything financially. The order however is sacred. If an order was deleted you could lose financial data and even fulfillment information. Hence you don't want to be messing around with the order because you could be shooting yourself in the foot.
+# I view your cart as something that can be removed off the face of the planet and not effect much. Sure
+# people would be upset to add things back to their cart but at the end of the day it would not effect anything
+# financially. The order however is sacred. If an order was deleted you could lose financial data and even
+# fulfillment information. Hence you don't want to be messing around with the order because you could be
+# shooting yourself in the foot.
 #
-# By nature your cart has a lot of abandoned records. If the order and cart are separated you could very easily archive the carts without much worry. If your order is your cart the risk to do this would be too great. One small bug could cost you way too much.
+# By nature your cart has a lot of abandoned records. If the order and cart are separated you could very easily
+# archive the carts without much worry. If your order is your cart the risk to do this would be too great.
+# One small bug could cost you way too much.
 #
 # Now you have an extremely slim cart with a tremendous amount of functionality.
 
-#  ###Removing an item
+# == Removing an item
 #
-#  The when the item is removed from the cart instead of deleting the cart_item the item is changed to active = false.  This allows you to see the state of the item when it was removed from the cart.
+# The when the item is removed from the cart instead of deleting the cart_item the item is
+# changed to active = false.  This allows you to see the state of the item when it was removed from the cart.
 #
-#  ###Save for later
+# == Save for later
 #
-#  The when the item is marked for "save_for_later" in the cart now the state of the item is just changed to "save_for_later".  So adding it back to the cart is as easy as changing the state to "shopping_cart".
+# The when the item is marked for "save_for_later" in the cart now the state of the item is just changed to "save_for_later".
+# So adding it back to the cart is as easy as changing the state to "shopping_cart".
 #
-#    Take a look at [This Blog Post](http://www.ror-e.com/posts/29-e-commerce-tips-1-2-the-shopping-cart) for more details.
+# Take a look at [This Blog Post](http://www.ror-e.com/posts/29-e-commerce-tips-1-2-the-shopping-cart) for more details.
 
 
 # == Schema Information
