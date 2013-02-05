2aa5fcefd75bec25836d005256c6d02dd0e068ea
David Henner <drhenner@yahoo.com>
2013-02-04 17:55:18 -0800

    remove commented out code

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -9,7 +9,7 @@ describe Invoice, "requirements" do
 end
 
 describe Invoice, "instance methods" do
-  before(:each) do 
+  before(:each) do
     User.any_instance.stubs(:start_store_credits).returns(true)  ## simply speed up tests, no reason to have store_credit object
     @invoice = create(:invoice, :created_at => '2010-11-26 23:55:14')
   end
@@ -21,7 +21,7 @@ describe Invoice, "instance methods" do
       @invoice.order_ship_address_lines
     end
   end
-  
+
   context '.order_billing_address_lines' do
     it 'should display invoice created_at date in the correct format' do
       #@invoice.order.expects(:bill_address).returns(create(:address))
@@ -29,23 +29,23 @@ describe Invoice, "instance methods" do
       @invoice.order_billing_address_lines
     end
   end
-  
-  
-  
+
+
+
   #invoice_date(format = :us_date)
   context '.invoice_date' do
     it 'should display invoice created_at date in the correct format' do
       @invoice.invoice_date.should == '11/26/2010'
     end
   end
-  
+
   context '.number' do
     it 'should exist and not = id' do
       @invoice.number.should_not == @invoice.id
       @invoice.number.length.should > 3
     end
   end
-  
+
   context ".capture_complete_order" do
     it 'should create a CreditCardCapture transaction' do
       @invoice.stubs(:amount).returns(20.50)
@@ -53,21 +53,21 @@ describe Invoice, "instance methods" do
       @invoice.capture_complete_order.should be_true
       @invoice.order.user.transaction_ledgers.size.should == 2
     end
-    
+
     context ".authorize_complete_order" do
       it 'should create a CreditCardReceivePayment transaction' do
-        @invoice.stubs(:amount).returns(20.50)      
+        @invoice.stubs(:amount).returns(20.50)
         @invoice.authorize_complete_order.should be_true
         @invoice.order.user.transaction_ledgers.size.should == 2
         @invoice.capture_complete_order.should be_true
         @invoice.order.user.transaction_ledgers.size.should == 4
       end
-      
+
       context 'cancel_authorized_payment' do
         it 'should create a CreditCardReceivePayment transaction then cancel' do
-          @invoice.stubs(:amount).returns(20.50)      
+          @invoice.stubs(:amount).returns(20.50)
           @invoice.authorize_complete_order
-        
+
           @invoice.cancel_authorized_payment.should be_true
           @invoice.order.user.transaction_ledgers.size.should == 4
           revenue_credits = []
@@ -129,9 +129,6 @@ describe Invoice, "Class methods" do
     end
   end
 end
-#def Invoice.generate(order_id, charge_amount)
-#  Invoice.new(:order_id => order_id, :amount => charge_amount, :invoice_type => PURCHASE)
-#end
 
 describe Invoice, "#generate(order_id, charge_amount)" do
   it 'should find the invoice by number' do
@@ -159,14 +156,14 @@ describe Invoice, 'optimize' do
       invoice = create(:invoice)
       payment = create(:payment, :invoice => invoice, :action => 'authorization', :success => true, :confirmation_id => 'good')
       invoice.authorization_reference.should == payment.confirmation_id
-    end 
+    end
   end
 
   describe Invoice, ".succeeded?" do
     it 'will return a true if authorized or paid' do
       invoice = create(:invoice, :state => 'authorized')
       invoice.succeeded?.should be_true
-      
+
       invoice = create(:invoice, :state => 'paid')
       invoice.succeeded?.should be_true
     end
@@ -180,45 +177,15 @@ describe Invoice, ".integer_amount" do
   end
 end
 
-
-#def authorize_payment(credit_card, options = {})
-#  options[:number] ||= unique_order_number
-#  transaction do
-#    authorization = Payment.authorize(integer_amount, credit_card, options)
-#    payments.push(authorization)
-#    if authorization.success?
-#      payment_authorized!
-#      authorize_complete_order
-#    else
-#      transaction_declined!
-#    end
-#    authorization
-#  end
-#end
-
 describe Invoice, ".authorize_payment(credit_card, options = {})" do
   pending "test for authorize_payment(credit_card, options = {})"
 end
-
-#def capture_payment(options = {})
-#  transaction do
-#    capture = Payment.capture(integer_amount, authorization_reference, options)
-#    payments.push(capture)
-#    if capture.success?
-#      payment_captured!
-#      capture_complete_order
-#    else
-#      transaction_declined!
-#    end
-#    capture
-#  end
-#end
 
 describe Invoice, ".capture_payment(options = {})" do
   pending "test for capture_payment(options = {})"
 end
 
-describe Invoice, ".user_id" do 
+describe Invoice, ".user_id" do
   it 'should give the orders user_id' do
     invoice = create(:invoice)
     invoice.user_id.should == invoice.order.user_id
