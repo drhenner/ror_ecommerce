d0538663baa9f6dcadfa260d2308722ecf070f57
David Henner <drhenner@yahoo.com>
2013-02-04 16:07:27 -0800

    less duplication

--- app/models/transaction_accountable.rb
+++ app/models/transaction_accountable.rb
@@ -1,12 +1,15 @@
 module TransactionAccountable
 
   def new_credit(transaction_account_id, credit_amount, at = Time.zone.now)
-    credit = self.transaction_ledgers.new(:transaction_account_id => transaction_account_id, :debit => 0, :credit => credit_amount, :period => "#{at.month}-#{at.year}")
-    credit
+    credit = new_transaction(transaction_account_id, 0, credit_amount, at)
   end
 
   def new_debit(transaction_account_id, debit_amount, at = Time.zone.now)
-    debit = self.transaction_ledgers.new(:transaction_account_id => transaction_account_id, :debit => 0, :credit => debit_amount, :period => "#{at.month}-#{at.year}")
-    debit
+    debit = new_transaction(transaction_account_id, debit_amount, 0, at)
   end
+
+  private
+    def new_transaction(transaction_account_id, debit_amount, credit_amount, at)
+      self.transaction_ledgers.new(:transaction_account_id => transaction_account_id, :debit => debit_amount, :credit => credit_amount, :period => "#{at.month}-#{at.year}")
+    end
 end
