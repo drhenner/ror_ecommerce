c399168a049694ce6be5d192cb048ffd6ae0f8ca
David Henner <drhenner@yahoo.com>
2013-02-07 11:35:11 -0800

    demo uses this exact code so let the activation email work…

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,7 +83,7 @@ Hadean::Application.configure do
   # Enable serving of images, stylesheets, and javascripts from an asset server
   # config.action_controller.asset_host = "http://assets.example.com"
 
-  config.action_mailer.default_url_options = { :host => 'rore.example.com' }
+  config.action_mailer.default_url_options = { :host => 'ror-e.herokuapp.com' }
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
