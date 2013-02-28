4272bdc9e10c854f535b7f5e776975791a8953b5
David Henner <drhenner@yahoo.com>
2013-02-28 01:51:40 -0800

    old fix for authlogic is not longer needed

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,7 +2,4 @@ class UserSession < Authlogic::Session::Base
   # specify configuration here, such as:
   # logout_on_timeout true
   # ...many more options in the documentation
-  def to_key
-     self.keys.to_a
-  end
 end
