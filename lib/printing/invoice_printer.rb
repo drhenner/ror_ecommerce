08a0927633c2cef2498ff750cb19daf9f39da48a
David Henner <drhenner@yahoo.com>
2013-03-06 16:50:30 -0800

    changes to invoice pdf

--- lib/printing/invoice_printer.rb
+++ lib/printing/invoice_printer.rb
@@ -11,20 +11,18 @@ module InvoicePrinter
     return document
   end # end of print_form method
 
-  def print_invoice_background(pdf, png_background = "#{Rails.root}/public/images/invoice_template.png")
+  def print_invoice_background(pdf, png_background = "https://s3.amazonaws.com/#{ENV['FOG_DIRECTORY']}/invoice_template.png")
     pdf.bounding_box([10,730], :width => 530) do
-      pdf.image png_background, :width => 519 if png_background
+      pdf.image open(png_background), :width => 519 if png_background && ENV['FOG_DIRECTORY'].present?
     end
-    print_logo(pdf)
-  end
-
-  def print_logo(pdf)
-    pdf.bounding_box([20,720], :width => 120) do
-      pdf.image "#{Rails.root}/public/images/logos/#{Image::MAIN_LOGO}"
-    end
-  rescue
-    pdf.bounding_box([20,720], :width => 120) do
-      pdf.draw_text "LOGO", {:size => 32, :at => [100, 100]}
+    begin
+      pdf.bounding_box([20,720], :width => 120) do
+        pdf.image open("https://s3.amazonaws.com/#{ENV['FOG_DIRECTORY']}/logos/logo.png") if ENV['FOG_DIRECTORY'].present?
+      end
+    rescue
+      pdf.bounding_box([20,720], :width => 120) do
+        pdf.draw_text "LOGO", {:size => 32, :at => [50,50]}
+      end
     end
   end
 
@@ -43,49 +41,63 @@ module InvoicePrinter
       pdf.draw_text item.variant.product_name,      {:at => [130, 420 - (i * 50) ]}
       pdf.draw_text number_to_currency(item.price), {:at => [470, 420 - (i * 50) ]}
     end
-    invoice.order.shipping_rates.each_with_index do |shipping_rate, i|
-      pdf.draw_text 'Shipping',      {:at => [130, 270 - (i * 15) ]}
-      pdf.draw_text shipping_rate.rate,      {:at => [470, 270 - (i * 15) ]}
+    inum = 0
+    #invoice.order.shipping_rates.each_with_index do |shipping_rate, i|
+      pdf.draw_text 'Shipping',      {:at => [130, 270 - (inum * 15) ]}
+      pdf.draw_text number_to_currency(invoice.order.shipping_charges),      {:at => [470, 270 - (inum * 15) ]}
+    #end
+    if invoice.order.coupon_id
+        pdf.draw_text 'Coupon',      {:at => [130, 257 - (inum * 15) ]}
+        pdf.draw_text number_to_currency(invoice.order.coupon_amount),      {:at => [470, 257 - (inum * 15) ]}
     end
-    pdf.draw_text invoice.order.total_tax_charges,      {:at => [480, 175], :size => 10 }
+    if invoice.order.deal_amount && invoice.order.deal_amount > 0.0
+        pdf.draw_text 'Coupon',      {:at => [130, 244 - (inum * 15) ]}
+        pdf.draw_text number_to_currency(invoice.order.deal_amount),      {:at => [470, 244 - (inum * 15) ]}
+    end
+    pdf.draw_text number_to_currency(invoice.order.total_tax_charges.round_at(2)),      {:at => [480, 175], :size => 10 }
+  end
+
+  def next_num
+    @num ||= 0
+    @num = @num + 1
+  end
+
+  def reset_num
+    @num = 0
   end
 
   def new_page(pdf, invoice, start_new_page = true)
     pdf.start_new_page() if start_new_page
     pdf.font "#{Rails.root}/lib/printing/fonts/DejaVuSans.ttf"
     print_invoice_background(pdf)
-    new_page_bounding_box(pdf, invoice)
-  end
 
-  def new_page_bounding_box(pdf, invoice)
     pdf.bounding_box([0,400], :width => 612) do
       @invoice_yml.each do |info|
-        display_string = invoice.send(info.last['method'].to_sym)
         if info.last['multiple_lines']
-          pdf.bounding_box( bounded_at(info.last), bounded_by(info.last) ) do
-            print_lines(pdf, display_string )
+          pdf.bounding_box(   [  info.last['arguements']['bounded_at'][0].to_i,
+                                info.last['arguements']['bounded_at'][1].to_i
+                              ],
+                                :width => info.last['arguements']['bounded_by'][0].to_i,
+                                :height => info.last['arguements']['bounded_by'][1].to_i
+                              ) do
+
+            print_lines(pdf, invoice.send(info.last['method'].to_sym) )
           end
         else
-          print_line(pdf, display_string, info.last )
+          print_line(pdf, invoice.send(info.last['method'].to_sym), info.last )
         end
       end
     end
   end
 
-
-  # [Array]  [X-location, Y-location]
-  def bounded_at(pdf_info)
-    [pdf_info['arguements']['bounded_at'][0].to_i, pdf_info['arguements']['bounded_at'][1].to_i]
-  end
-  # [Hash]  {:width => 1, :height => 2 }
-  def bounded_by(pdf_info)
-    {:width => pdf_info['arguements']['bounded_by'][0].to_i, :height => pdf_info['arguements']['bounded_by'][1].to_i}
-  end
-
   def print_bounding_box(pdf, invoice, pdf_info)
-    pdf.bounding_box( bounded_at(pdf_info),bounded_by(pdf_info) ) do
+    pdf.bounding_box( [ pdf_info['arguements']['bounded_at'][0].to_i,
+                        pdf_info['arguements']['bounded_at'][1].to_i
+                      ],
+                      :width  => pdf_info['arguements']['bounded_by'][0].to_i,
+                      :height => pdf_info['arguements']['bounded_by'][1].to_i
+                    ) do
         pdf.text invoice.send( pdf_info['arguements']['method'].to_sym )
     end
   end
-
 end
