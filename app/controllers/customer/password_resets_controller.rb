3274721a8d868738373b2a4e705a29155de5696b
David Henner <drhenner@yahoo.com>
2013-03-19 15:41:19 -0700

    gave bad reset message when the link is valid

--- app/controllers/customer/password_resets_controller.rb
+++ app/controllers/customer/password_resets_controller.rb
@@ -39,9 +39,10 @@ class Customer::PasswordResetsController < ApplicationController
     protected
 
     def load_user_using_perishable_token
-      @user = User.find_by_perishable_token( params[:id])
-      flash[:notice] = 'The link you used in no longer valid.  Click the password reset link to get a new link to reset your password.'
-      redirect_to login_url and return unless @user
+      unless @user = User.find_by_perishable_token( params[:id])
+        flash[:notice] = 'The link you used in no longer valid.  Click the password reset link to get a new link to reset your password.'
+        redirect_to login_url and return
+      end
     end
 
 end
