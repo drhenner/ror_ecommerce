48ccbc461307c11c3c77e965d9eccbb81d72e105
David Henner <drhenner@yahoo.com>
2013-04-14 19:59:12 -0700

    set up factories properly

--- spec/factories/newsletters.rb
+++ spec/factories/newsletters.rb
@@ -1,7 +1,7 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :newsletter, :class => 'Newsletters' do
+  factory :newsletter do
     name "MyString"
     autosubscribe false
   end
--- spec/factories/users_newsletters.rb
+++ spec/factories/users_newsletters.rb
@@ -1,8 +1,8 @@
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
-  factory :users_newsletter, :class => 'UsersNewsletters' do
-    user_id 1
-    newsletter_id 1
+  factory :users_newsletter do
+    user { |c| c.association(:user) }
+    newsletter_id 1#{ Newsletter.first }
   end
 end
