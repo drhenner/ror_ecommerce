16ea81f4865fe9239ecf7b35decdba698916873d
David Henner <drhenner@yahoo.com>
2013-04-14 19:31:31 -0700

    comment

--- config/initializers/resque_auth.rb
+++ config/initializers/resque_auth.rb
@@ -1,5 +1,6 @@
 if Settings.uses_resque_for_background_emails && defined?(Resque)
   Resque::Server.use(Rack::Auth::Basic) do |user, password|
+    # username and password for looking at resque admin
     [user, password] == ["ror-e", "ror-eP@sSw0Rd"]
   end
 end
