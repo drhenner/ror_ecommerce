05234ffcd510b56dc7a816f4aec8a4b5b55a50e3
David Henner <drhenner@yahoo.com>
2013-04-17 21:43:15 -0700

    caching tax rate has issues with test.. shouldn't be an issue in prod.  never deleting prod db

--- app/models/tax_rate.rb
+++ app/models/tax_rate.rb
@@ -49,10 +49,10 @@ class TaxRate < ActiveRecord::Base
   end
 
   def self.active_at_ids(date = Time.zone.now.to_date)
-    Rails.cache.fetch("TaxRate-#{I18n.t(:company)}-active_at_ids-#{date}", :expires_in => 23.hours) do
+    #Rails.cache.fetch("TaxRate-#{I18n.t(:company)}-active_at_ids-#{date}", :expires_in => 23.hours) do
       TaxRate.where(["tax_rates.start_date <= ? AND
              (end_date > ? OR end_date IS NULL)", date.to_s(:db), date.to_s(:db)]).pluck(:id)
-    end
+    #end
   end
 
   private
