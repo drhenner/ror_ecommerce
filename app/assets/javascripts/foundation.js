cf3a9f503e11688621d7a3e9e2b05929b701d886
David Henner <drhenner@yahoo.com>
2013-05-03 20:35:16 -0700

    upgrade ZURB and product page

--- app/assets/javascripts/foundation.js
+++ app/assets/javascripts/foundation.js
@@ -1,2 +1,4 @@
 //= require modernizr.foundation
+//= require zepto
+//= require_tree  ./foundation_init
 //= require_tree  ./foundation
---
+++ app/assets/javascripts/product_page.js
@@ -0,0 +1,30 @@
+var Hadean = window.Hadean || { };
+if (typeof Hadean.Product == "undefined") {
+    Hadean.Product = {};
+}
+dd = null;
+if (typeof Hadean.Product.tabs == "undefined") {
+  Hadean.Product.tabs = {
+    newFormId : '#new_cart_item',
+    addToCart : true,
+
+    initialize      : function() {
+      $('#product_tabs .section-container section ').click(function() {
+        setTimeout('Hadean.Product.tabs.updateProductTabs()', 100);
+      })
+      Hadean.Product.tabs.updateProductTabs();
+    },
+    updateProductTabs : function() {
+      var heightOfTabContent = $('#product_tabs .section-container .section.active').height();
+      if ( heightOfTabContent ) {
+        $('#product_tabs .section-container').height(heightOfTabContent + 75);
+      } else {
+        $('#product_tabs .section-container').height(170);
+        setTimeout('Hadean.Product.tabs.updateProductTabs()', 200);
+      }
+    }
+  };
+  jQuery(function() {
+    Hadean.Product.tabs.initialize();
+  });
+};
