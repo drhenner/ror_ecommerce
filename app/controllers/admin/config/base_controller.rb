03663d7a14d2b52c865216fc43f903101087d94c
David Henner <drhenner@yahoo.com>
2013-05-17 12:07:39 -0700

    wording in comment

--- app/controllers/admin/config/base_controller.rb
+++ app/controllers/admin/config/base_controller.rb
@@ -1,5 +1,5 @@
 class Admin::Config::BaseController < Admin::BaseController
   skip_before_filter :verify_admin
-  before_filter :verify_super_admin  # ONLY SUPER ADMINS should touch this
+  before_filter :verify_super_admin  # ONLY SUPER ADMINS should see this
   layout 'admin'
 end
