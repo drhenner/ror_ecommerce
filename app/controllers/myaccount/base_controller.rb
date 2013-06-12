aedef1003f70f7fd776fd362b25de7e13d90ba23
David Henner <drhenner@yahoo.com>
2013-06-12 08:42:21 -0700

    expire cache

--- app/controllers/myaccount/base_controller.rb
+++ app/controllers/myaccount/base_controller.rb
@@ -1,6 +1,7 @@
 class Myaccount::BaseController < ApplicationController
   helper_method :selected_myaccount_tab
   before_filter :require_user
+  before_filter :expire_all_browser_cache
 
   protected
   def myaccount_tab
