25119278235ea2776ffa0dbdb121012f2ea6ec14
David Henner <drhenner@yahoo.com>
2013-07-17 22:12:17 -0700

    remove duplication

--- app/controllers/admin/reports/overviews_controller.rb
+++ app/controllers/admin/reports/overviews_controller.rb
@@ -10,36 +10,4 @@ class Admin::Reports::OverviewsController < Admin::Reports::BaseController
     @beginning_number_of_cart_items = CartItem.before(start_time).last ? CartItem.before(start_time).last.id : 0
   end
 
-  private
-
-    def set_time_range
-      if params[:start_date].present?
-        @start_time = Time.parse(params[:start_date])
-      else
-        @start_time = Chronic.parse('last week').beginning_of_week
-      end
-      set_end_time
-    end
-
-    def set_end_time
-      @end_time = case params[:commit]
-      when 'Daily'
-        start_time + 1.day
-      when 'Weekly'
-        start_time + 1.week
-      when 'Monthly'
-        start_time + 1.month
-      else
-        start_time + 1.week
-      end
-    end
-
-    def start_time
-      @start_time
-    end
-
-    def end_time
-      @end_time
-    end
-
 end
