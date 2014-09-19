e542f87ee95ca3bc55e2e63cc88740c2461b9289
David Henner <drhenner@yahoo.com>
2014-09-18 18:47:48 -0700

    add test to cover security issue to make sure it is not an issue in your setup

--- spec/controllers/customer/password_resets_controller_spec.rb
+++ spec/controllers/customer/password_resets_controller_spec.rb
@@ -4,7 +4,7 @@ describe Customer::PasswordResetsController do
   render_views
 
   it "new action should render new template" do
-    @user = create(:user)
+    @user = FactoryGirl.create(:user)
     get :new
     response.should render_template(:new)
   end
@@ -12,33 +12,39 @@ describe Customer::PasswordResetsController do
   it "create action should render new template when model is invalid" do
     User.any_instance.stubs(:valid?).returns(false)
     User.any_instance.stubs(:find_by_email).returns(nil)
-    post :create, :user => {:email => 'wertyuvc'}
+    post :create, user: { email: 'wertyuvc' }
     response.should render_template(:new)
   end
 
   it "create action should redirect when model is valid" do
-    @user = create(:user)
+    @user = FactoryGirl.create(:user)
     User.any_instance.stubs(:valid?).returns(true)
     User.any_instance.stubs(:find_by_email).returns(@user)
-    post :create, :user => {:email => @user.email}
+    post :create, user: { email: @user.email }
     #response.should render_template('/customer/password_resets/confirmation')
   end
 
   it "edit action should render edit template" do
-    @user = create(:user)
+    @user = FactoryGirl.create(:user)
     get :edit, :id => @user.perishable_token
     response.should render_template(:edit)
   end
 
+  it "edit action should render edit template" do
+    @user = FactoryGirl.create(:user)
+    get :edit, id: 0
+    expect(response).not_to render_template(:edit)
+  end
+
   it "update action should render edit template when model is invalid" do
-    @user = create(:user)
+    @user = FactoryGirl.create(:user)
     User.any_instance.stubs(:valid?).returns(false)
-    put :update, :id => @user.perishable_token, :user => {:password => 'testPWD123', :password_confirmation => 'testPWD123'}
+    put :update, id: @user.perishable_token, user: { password: 'testPWD123', password_confirmation: 'testPWD123'}
     response.should render_template(:edit)
   end
 
   it "update action should redirect when model is valid" do
-    @user = create(:user)
+    @user = FactoryGirl.create(:user)
     User.any_instance.stubs(:valid?).returns(true)
     put :update, id: @user.perishable_token, user: { password: 'testPWD123', password_confirmation: 'testPWD123'}
     response.should redirect_to(login_url)
