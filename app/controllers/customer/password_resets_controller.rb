25fe5ebb2f193978e9f9967c9dfe6be5716e8650
David Henner <drhenner@yahoo.com>
2014-09-18 18:46:31 -0700

    add to_s just in case

--- app/controllers/customer/password_resets_controller.rb
+++ app/controllers/customer/password_resets_controller.rb
@@ -8,16 +8,16 @@ class Customer::PasswordResetsController < ApplicationController
     end
 
     def create
-        @user = User.find_by_email(params[:user][:email])
-        if @user
-          @user.deliver_password_reset_instructions!
-          flash[:notice] = 'Instructions to reset your password have been emailed.'
-          render :template => '/customer/password_resets/confirmation'
-        else
-          @user = User.new
-          flash[:notice] = 'No user was found with that email address'
-          render :action => 'new'
-        end
+      @user = User.find_by_email(params[:user][:email])
+      if @user
+        @user.deliver_password_reset_instructions!
+        flash[:notice] = 'Instructions to reset your password have been emailed.'
+        render :template => '/customer/password_resets/confirmation'
+      else
+        @user = User.new
+        flash[:notice] = 'No user was found with that email address'
+        render :action => 'new'
+      end
     end
 
     def edit
@@ -39,7 +39,7 @@ class Customer::PasswordResetsController < ApplicationController
     protected
 
     def load_user_using_perishable_token
-      unless @user = User.find_by_perishable_token( params[:id])
+      unless @user = User.find_by_perishable_token( params[:id].to_s )
         flash[:notice] = 'The link you used in no longer valid.  Click the password reset link to get a new link to reset your password.'
         redirect_to login_url and return
       end
