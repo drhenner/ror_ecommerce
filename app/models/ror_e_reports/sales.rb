627611f5835e3708d882479902c5f51549de9d2e
David Henner <drhenner@yahoo.com>
2014-11-30 23:58:19 -0800

    bug at end of month (at few hours) displaying reports

--- app/models/ror_e_reports/sales.rb
+++ app/models/ror_e_reports/sales.rb
@@ -13,26 +13,26 @@ module RorEReports
       summary = baseline_report
       @orders.each do |order|
         date = date_for(order.completed_at)
-        summary[date][:amount] += order.first_invoice_amount
-        summary[date][:sales] += 1
+        summary[date][:amount] += order.first_invoice_amount if date
+        summary[date][:sales]  += 1                          if date
       end
       summary.values
     end
 
     def date_for(at)
-      report_dates.detect{|key, date_string| at <= key }.try(:first)
+      report_dates.detect{|key, date_string| at.utc.to_date <= key.to_date }.try(:first)
     end
 
     def report_dates
-      @report_dates ||= (@number_of_weeks).times.inject({}) do |h, i|
-        h[(@start_time + i.weeks)] = (@start_time + i.weeks).strftime("%Y-%m-%d")
+      @report_dates ||= (@number_of_weeks + 1).times.inject({}) do |h, i|
+        h[(@start_time + i.weeks).utc.to_date] = (@start_time + i.weeks).strftime("%Y-%m-%d")
         h
       end
     end
 
     def baseline_report
-      @baseline_report ||= (@number_of_weeks).times.inject({}) do |h, i|
-        h[(@start_time + i.weeks)] = {
+      @baseline_report ||= (@number_of_weeks + 1).times.inject({}) do |h, i|
+        h[(@start_time + i.weeks).utc.to_date] = {
           date:   (@start_time + i.weeks).strftime("%Y-%m-%d"),
           amount: 0,
           sales:  0}
