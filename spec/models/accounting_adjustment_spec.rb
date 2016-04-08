1ae35155f0ef1c7de1dfda58bfdd42706799dea6
David Henner <drhenner@yahoo.com>
2016-04-08 15:24:23 -0700

    fix test

--- spec/models/accounting_adjustment_spec.rb
+++ spec/models/accounting_adjustment_spec.rb
@@ -2,8 +2,10 @@ require 'spec_helper'
 
 describe AccountingAdjustment do
   context '#adjust_stock(inventory, qty_to_add, return_amount)' do
+    let(:inventory) { FactoryGirl.create(:inventory, :count_on_hand => 30000) }
+    let!(:variant)   { FactoryGirl.create(:variant,   :inventory => inventory) }
+
     it "should update count on hand" do
-      inventory = FactoryGirl.create(:inventory, :count_on_hand => 30000)
       AccountingAdjustment.adjust_stock(inventory, 1000, 12.00)
       inventory.reload
       expect(inventory.count_on_hand).to eq 31000
