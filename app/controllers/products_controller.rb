fa45b75fd030266d898b4b7af29b55663337e90f
David Henner <drhenner@yahoo.com>
2017-08-14 20:42:23 -0700

    add pagination to products page

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -8,9 +8,10 @@ class ProductsController < ApplicationController
       product_types = product_type.self_and_descendants.map(&:id)
     end
     if product_types
-      @products = products.where(product_type_id: product_types)
+      @products = products.where(product_type_id: product_types).
+                           paginate(page: pagination_page, per_page: pagination_rows)
     else
-      @products = products
+      @products = products.paginate(page: pagination_page, per_page: pagination_rows)
     end
   end
 
@@ -38,5 +39,10 @@ class ProductsController < ApplicationController
 
   def featured_product_types
     [ProductType::FEATURED_TYPE_ID]
+  end
+
+  def pagination_rows
+    params[:rows] ||= 60
+    params[:rows].to_i
   end
 end
